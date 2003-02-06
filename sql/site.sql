55f0b45f081d885868168dfadea09ca81d2f149a
adam <adam@689d98c0-9023-0410-9c0b-f79783857903>
2003-02-06 02:00:57 +0000

    Added 'creation_date' field
    git-svn-id: http://svn.krangcms.com/trunk/krang@104 689d98c0-9023-0410-9c0b-f79783857903

---
+++ sql/site.sql
@@ -0,0 +1,15 @@
+--
+-- Table structure for table 'site'
+--
+
+DROP TABLE IF EXISTS site;
+CREATE TABLE site (
+       site_id		int UNSIGNED NOT NULL AUTO_INCREMENT,
+       url		varchar(255) NOT NULL,
+       preview_url	varchar(255),
+       publish_url	varchar(255),
+       preview_path	varchar(255),
+       publish_path	varchar(255),
+       creation_date    datetime NOT NULL,
+       primary key (site_id)
+) TYPE=MyISAM;
