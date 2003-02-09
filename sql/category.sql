22cecc47064433ab02b424d80d7a9300542bd3c2
adam <adam@689d98c0-9023-0410-9c0b-f79783857903>
2003-02-09 10:08:12 +0000

    renamed fields to support changes in module
    git-svn-id: http://svn.krangcms.com/trunk/krang@126 689d98c0-9023-0410-9c0b-f79783857903

--- sql/category.sql
+++ sql/category.sql
@@ -6,11 +6,13 @@ DROP TABLE IF EXISTS category;
 CREATE TABLE category (
         category_id	int UNSIGNED NOT NULL AUTO_INCREMENT,
         element_id      int UNSIGNED,
+        name		varchar(255) NOT NULL,
         parent_id	int UNSIGNED,
-        path		varchar(255) NOT NULL,
         site_id		int UNSIGNED,
+        url		varchar(255) NOT NULL,
         PRIMARY KEY (category_id),
         KEY (element_id),
+        KEY (name),
         KEY (parent_id),
         KEY (site_id)
 ) TYPE=MyISAM;
