8f890d5767a2174de880a1aff40e28f2b1a1a34c
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2003-03-04 23:46:54 +0000

    New Krang.pm with all the naughty bits removed.
    git-svn-id: http://svn.krangcms.com/trunk/krang@261 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang.pm
+++ lib/Krang.pm
@@ -6,98 +6,21 @@ our $VERSION = "0.001";
 
 =head1 NAME
 
-Krang - loader for the Krang system
+Krang - the Krang publishing system
 
 =head1 SYNOPSIS
 
   use Krang;
+  print "This is Krang version ", $Krang::VERSION, "!\n";
 
 =head1 DESCRIPTION
 
-This module exists only to load and configure other modules.  All
-Krang clients should load this module first before using other Krang
-modules.
-
-When loaded from outside Apache/mod_perl, this module will activate
-the first instance defined in F<krang.conf> by calling:
-
-  Krang::Conf->instance((Krang::Conf->instances())[0]);
-
-You can override this behavior by setting the KRANG_INSTANCE
-environment variable.
-
-When loaded from Apache/mod_perl no default instance is set since
-Krang::Handler will set the instance at the start of each request
-depending on the vhost accessed.
+Krang.pm contains the Krang version number and nothing else.
 
 =head1 INTERFACE
 
 None.
 
-=head1 TODO
-
-The way the session setup for non-Apache usage gets user_id 1 with no
-authentication is mighty hinky.
-
 =cut
-
-use Carp qw(verbose croak); # turn croaks into confesses
-
-# load base Krang modules
-use Krang::Conf qw(ElementSet);
-use Krang::Log qw(critical info debug);
-use Krang::ElementLibrary;
-use Krang::Session qw(%session);
-
-# load all configured element sets
-BEGIN {
-    foreach my $instance (Krang::Conf->instances()) {
-        Krang::Conf->instance($instance);
-        Krang::ElementLibrary->load_set(set => ElementSet());
-    }
-    Krang::Conf->instance(undef);
-}
-
-
-BEGIN {
-
-    # Set up environment for non-MOD_PERL operation
-    # Set instance, load/unload session
-    unless ($ENV{MOD_PERL}) {
-
-        # Set Krang instance if not running under mod_perl
-        my $instance = exists $ENV{KRANG_INSTANCE} ? 
-          $ENV{KRANG_INSTANCE} : (Krang::Conf->instances())[0];
-        debug("Krang.pm:  Setting instance to '$instance'");
-        Krang::Conf->instance($instance);
-
-
-        # open up a temporary session for cli scripts
-        unless ( ($ENV{GATEWAY_INTERFACE} || '') =~ /^CGI/ ) {
-            my $session_id = Krang::Session->create();
-
-            # get a user_id from KRANG_USER_ID or default to 1
-            my $user_id = exists $ENV{KRANG_USER_ID} ? $ENV{KRANG_USER_ID} : 1;
-            $session{user_id} = $user_id;
-            debug "Setting user_id to $user_id";
-
-            # arrange for it to be deleted at process end
-            eval "END { Krang::Session->delete() }";
-        }
-    }
-}
-
-# setup die handler to write fatal errors to the log before really
-# dying.  In Apache/mod_perl, this is done with an eval {} in
-# Krang::Handler.
-unless ($ENV{MOD_PERL}) {
-    $SIG{__DIE__} = sub {
-        return if $^S;   # ignore die inside an eval
-        critical $_[0];
-        die $_[0];
-    };
-}
-
-info("Krang v$VERSION loaded");
 
 1;
