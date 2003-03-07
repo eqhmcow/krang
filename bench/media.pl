18eaba243e3148747afba06aa873394aa96fc671
mvella <mvella@689d98c0-9023-0410-9c0b-f79783857903>
2003-03-07 04:02:47 +0000

    fixed media.pl bench to work with categories and unique urls
    git-svn-id: http://svn.krangcms.com/trunk/krang@281 689d98c0-9023-0410-9c0b-f79783857903

--- bench/media.pl
+++ bench/media.pl
@@ -1,25 +1,40 @@
 use strict;
 use warnings;
-use Krang;
+use Krang::Script;
 use Krang::Conf qw (KrangRoot);
 use File::Spec::Functions qw(catdir catfile);
 use FileHandle;
 use Krang::Benchmark qw(run_benchmark);
 use Krang::Media;
+use Krang::Category;
+use Krang::Site;
 use File::Path;
 
 my $count = 1000;
 my $filepath = catfile(KrangRoot,'t','media','krang.jpg');
 my $media;
 
+# set up site and category
+my $site = Krang::Site->new(preview_path => './sites/test1/preview/',
+                            preview_url => 'preview.testsite1.com',
+                            publish_path => './sites/test1/',
+                            url => 'testsite1.com');
+$site->save();
+
+my ($category) = Krang::Category->find(site_id => $site->site_id());
+
+my $category_id = $category->category_id();
+
+my $i = 1;
+
 run_benchmark(module => 'Krang::Media',
               name   => 'new, upload, save',
               count  => $count,               
               code   =>
 sub {
-    $media = Krang::Media->new(title => 'test media object', category_id => 1);
+    $media = Krang::Media->new(title => 'test media object', category_id => $category_id);
     my $fh = new FileHandle $filepath;
-    $media->upload_file(filename => 'krang.jpg', filehandle => $fh);
+    $media->upload_file(filename => $i++."krang.jpg", filehandle => $fh);
     $media->save();
 } );
 
@@ -27,7 +42,7 @@ sub {
 
 my @media_object;
 
-my $i = $media->media_id() - ($count - 1);
+$i = $media->media_id() - ($count - 1);
 run_benchmark(module => 'Krang::Media',
               name   => 'find',
               count  => $count,               
@@ -113,3 +128,8 @@ run_benchmark(module => 'Krang::Media',
 ###################################
 
 rmtree(catdir(KrangRoot,'data','media')); 
+
+# delete categories and site
+$category->delete();
+$site->delete();
+
