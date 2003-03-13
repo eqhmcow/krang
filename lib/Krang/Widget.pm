343307256d0af3d6b3ba337c7da1332b1e79b528
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2003-03-13 18:47:07 +0000

    Added a few parameters to category_chooser() to support use in the Story edit screen
    git-svn-id: http://svn.krangcms.com/trunk/krang@339 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/Widget.pm
+++ lib/Krang/Widget.pm
@@ -33,9 +33,13 @@ This modules exports a set of generally useful CGI widgets.
 
 Returns a block of HTML implementing the standard Krang category
 chooser.  The C<name> and C<query> parameters are required.
+
 Optionally, an 'onchange' handler can be set to the name of a
 javascript function that will be called when the user picks a
-category.
+category.  Setting the 'label' variable will change the label on the
+button which defaults to 'Choose'.  Finally, setting 'display' to
+false will supress displaying the chosen category URL next to the
+button.
 
 The template for the category chooser is located in
 F<Widget/category_chooser.tmpl>.
@@ -44,15 +48,17 @@ F<Widget/category_chooser.tmpl>.
 
 sub category_chooser {
     my %args = @_;
-    my ($name, $query, $onchange) = @args{qw(name query onchange)};
+    my ($name, $query, $label, $display, $onchange) =
+      @args{qw(name query label display onchange)};
     croak("Missing required args: name and query")
       unless $name and $query;
 
     my $template = HTML::Template->new(filename => 
-                                       catfile(KrangRoot, "templates",
-                                               "Widget", 
-                                               "category_chooser.tmpl"),
-                                       cache => 1);
+                                         catfile(KrangRoot, "templates",
+                                                 "Widget", 
+                                                 "category_chooser.tmpl"),
+                                       cache   => 1,
+                                      );
 
     my $category_id = $query->param($name) || 0;
                                        
@@ -74,7 +80,9 @@ sub category_chooser {
                              });
     }
     $template->param(category_loop => \@category_loop,
-                     name          => $name);
+                     name          => $name,
+                     label         => $label || 'Choose',
+                     display       => defined $display ? $display : 1);
 
     return $template->output();
 }
