418468481056f0c501cc65a7743ada3adf2cd4ba
mvella <mvella@689d98c0-9023-0410-9c0b-f79783857903>
2003-05-06 21:13:05 +0000

    pretty much finished with Krang::Desk, moving on to Krang::CGI::DeskAdmin
    git-svn-id: http://svn.krangcms.com/trunk/krang@741 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/Desk.pm
+++ lib/Krang/Desk.pm
@@ -11,14 +11,34 @@ Krang::Desk - Krang Desk API
 
 =head1 SYNOPSIS
 
-  use Krang::Desk;
+    use Krang::Desk;
 
-  # add a new desk, defaulting to next available slot in order
-  my $desk = Krang::Desk->new( name => 'Publish');
+    # add a new desk, defaulting to next available slot in order
+    my $desk = Krang::Desk->new( name => 'Publish');
 
+    # return desk id
+    my $desk_id = $desk->desk_id;
+    
+    # return desk order
+    my $desk_order = $desk->order;
+
+    # create another desk, this time choosing order of
+    # another desk, effectively making order of other
+    # desk + 1
+    my $desk2 = Krang::Desk->new( name => 'Print', order => $desk->order );
+
+    # return desk objects with name 'Publish'
+    my @desks = Krang::Desk->find( name => 'Publish' );
+
+    # reorder desks (in this case switch order of the two we have created)
+    Krang::Desk->reorder(   $desk[0]->desk_id => $desk2->order,
+                            $desk2->desk_id => $desk[0]->order );
+    
+    $desk->delete();
+     
 =head1 DESCRIPTION
 
-Krang::Desk provides methods to create, delete, and reorder desks.
+Krang::Desk provides methods to create, delete, find and reorder desks.
 
 =head1 INTERFACE
 
@@ -28,7 +48,7 @@ Krang::Desk provides methods to create, delete, and reorder desks.
 
 =item new()
 
-Add a new desk.  Takes the following parameters.
+Add a new desk and save it.  Takes the following parameters.
 
 =over 4
 
@@ -44,13 +64,19 @@ available slot in desk order.
 =back
 
 =cut
+use Krang::MethodMaker
+    new_with_init => 'new',
+    new_hash_init => 'hash_init',
+    get_set       => [ qw( name ord ) ],
+    get => [ qw( desk_id ) ];
 
-sub new {
-    my $class = shift;
+sub init {
+    my $self = shift;
     my %args = @_;
     croak(__PACKAGE__."->new - 'name' is a required parameter") if not $args{'name'};
 
-    my $self = bless {}, $class;
+    # finish the object
+    $self->hash_init(%args);
 
     # insert record into db
     $self->_insert(%args);
@@ -88,8 +114,158 @@ sub _insert {
 
     $self->{desk_id} = $dbh->{mysql_insertid}; 
     $self->{name} = $args{name};
-    $self->{order} = $args{order};
+    $self->{ord} = $args{order};
 
+}
+
+=item desk_id()
+
+Return desk id for desk object.
+
+=item name()
+
+Return desk name
+
+=item order()
+
+Return desk order. Please note that if desk order is shuffled in
+the database, you will have to reload the desk object to get an
+accurate order.
+
+=cut
+
+sub order {
+    my $self = shift;
+    return $self->{ord};
+}
+
+=item reorder()
+
+Reorder desks as specified.  Takes hash of desk_id => order pairs.
+
+=cut
+
+sub reorder {
+    my $self = shift;
+    my %args = @_;
+    my $dbh = dbh();
+
+    foreach my $desk_id (keys %args) {
+        $dbh->do('UPDATE desk set ord = ? where desk_id = ?', undef, $args{$desk_id}, $desk_id); 
+    }
+}
+
+=item find()
+
+Returns a list of desk objects, desk_ids, or count based on search 
+parameters. Valid params below:
+
+=over 4
+
+=item * name
+
+=item * order
+
+=item * desk_id
+
+=item * count
+
+If this argument is specified, the method will return a count of 
+the desks matching criteria.
+
+=item * ids_only
+
+Returns only desk_ids for the results found in the DB, not objects.
+
+=item * order_by
+
+Specify the field by means of which the results will be sorted.
+Defaults to 'order'.
+
+=item * order_desc
+
+If this is set to true, results will be sorted as descending 
+(default is ascending).
+
+=cut
+
+sub find {
+    my $self = shift;
+    my %args = @_;
+
+    my %valid_params = (    name => 1,
+                            order => 1,
+                            desk_id => 1,
+                            count => 1,
+                            ids_only => 1,
+                            order_by => 1,
+                            order_desc => 1
+                        );
+
+    # check for invalid params and croak if one is found
+    foreach my $param (keys %args) {
+        croak (__PACKAGE__."->find() - Invalid parameter '$param' called.") if not $valid_params{$param};
+    }
+
+    croak(__PACKAGE__ . "->find(): 'count' and 'ids_only' were supplied. " .
+          "Only one can be present.") if ($args{count} && $args{ids_only});
+
+    my $order_by = $args{order_by} || 'ord';
+    $order_by = 'ord' if ($order_by eq 'order');
+
+    my $order_dir = $args{order_desc} ? 'desc' : 'asc';
+
+    my $select;
+
+    # build select part of query
+    if ($args{count}) {
+        $select = 'select count(*)';
+    } elsif ($args{ids_only}) {
+        $select = 'select desk_id';
+    } else {
+        $select = 'select desk_id, name, ord';
+    }
+
+    my $where_clause;
+    my @where;
+    if ($args{desk_id}) {
+        $where_clause = "where desk_id = ?";
+        push @where, $args{desk_id};
+    }
+
+    if ($args{name}) {
+        $where_clause ? ($where_clause .= " and name = ?") : ($where_clause = "where name = ?");
+        push @where, $args{name};
+    }
+
+    if ($args{order}) {
+        $where_clause ? ($where_clause .= " and ord = ?") : ($where_clause = "where ord = ?");
+        push @where, $args{order};
+    }
+
+    my $query = "$select from desk $where_clause";
+    $query .= " order by $order_by $order_dir";
+
+    my $dbh = dbh();
+    my $sth = $dbh->prepare($query);
+    $sth->execute(@where) || croak("Unable to execute statement $query");
+
+    my @desk_object;
+
+    while (my $row = $sth->fetchrow_hashref()) {
+        my $obj;
+        if ($args{'count'}) {
+            return $row->{count};
+        } elsif ($args{'only_ids'}) {
+            $obj = $row->{media_id};
+        } else {
+            $obj = bless {%$row}, $self;
+        }
+        push (@desk_object,$obj);
+    }
+    $sth->finish();
+    return @desk_object;
+ 
 }
 
 =item delete()
@@ -111,7 +287,7 @@ sub delete {
 
     # find order if this is not object
     if ($is_object) {
-        $order = $self->{order};
+        $order = $self->{ord};
     } else {
         my $sth = $dbh->prepare('SELECT ord from desk where desk_id = ?');
         $sth->execute($desk_id);
--- t/desk.t
+++ t/desk.t
@@ -5,6 +5,35 @@ use Test::More qw(no_plan);
 
 BEGIN { use_ok('Krang::Desk'); }
 
-my $copy_desk = Krang::Desk->new( name => 'qop', order => 4 );
+my $copy_desk = Krang::Desk->new( name => 'copy_test' );
+END { $copy_desk->delete(); }
 
-$copy_desk->delete();
+# test simple fields
+is($copy_desk->name, 'copy_test');
+my $copy_desk_order = $copy_desk->order;
+my $copy_desk_id = $copy_desk->desk_id;
+
+my @desks = Krang::Desk->find( desk_id => $copy_desk_id );
+
+# field checks
+is($desks[0]->name, $copy_desk->name);
+is($desks[0]->order, $copy_desk->order);
+
+my $publish_desk = Krang::Desk->new( name => 'publish_test', order => $copy_desk->order);
+
+END { $publish_desk->delete; }
+
+# make sure orders are correct now
+@desks = Krang::Desk->find( desk_id => $copy_desk_id );
+is($desks[0]->order, ($copy_desk_order + 1));
+is($publish_desk->order, $copy_desk_order);
+
+Krang::Desk->reorder( $copy_desk_id => $publish_desk->order, $publish_desk->desk_id => $desks[0]->order );
+
+# check to see order has reversed
+@desks = Krang::Desk->find( desk_id => $copy_desk_id );
+is($desks[0]->order, $copy_desk_order);
+
+@desks = Krang::Desk->find( desk_id => $publish_desk->desk_id );
+is($desks[0]->order, ($copy_desk_order + 1));
+
