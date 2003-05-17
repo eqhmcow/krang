0ff825d6a43841e1361204b97bcfa756bcb03ac5
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2003-05-17 17:43:35 +0000

    Added code to force stack traces on uncaught exceptions.  No more mysterious 'Duplicate URL' errors\!
    git-svn-id: http://svn.krangcms.com/trunk/krang@887 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/ErrorHandler.pm
+++ lib/Krang/ErrorHandler.pm
@@ -3,6 +3,11 @@ use strict;
 use warnings;
 use Krang::Log qw(critical debug);
 
+# make Exception::Class errors print a stack trace when not caught
+use Exception::Class;
+Exception::Class::Base->Trace(1);
+
+# setup die() and warn() handlers
 BEGIN {
     $SIG{__DIE__} = sub {
         my $in_eval = 0;
@@ -41,6 +46,10 @@ When you use this module, $SIG{__DIE__} and $SIG{__WARN__} handlers
 are setup such that all die() and warn() messages are sent to
 Krang::Log.  This does not alter their operation - die() still really
 die()s and warn() still prints to STDERR.
+
+This module also turns on the Trace option for all Exception::Class
+usage in Krang.  This causes a stack trace to be printed when an
+unhandled exception is printed.
 
 Note that Krang::Script uses Krang::ErrorHandler, so test scripts
 don't need to use this module explicitely.  However, CGI scripts do
