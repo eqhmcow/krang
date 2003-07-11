21fb86ac143accead28b373562619e6c0fbcd731
mvella <mvella@689d98c0-9023-0410-9c0b-f79783857903>
2003-07-11 17:39:59 +0000

    changed the publish 100 stories, one at a time so they use Krang::Publisher instead of krang_publish
    git-svn-id: http://svn.krangcms.com/trunk/krang@1413 689d98c0-9023-0410-9c0b-f79783857903

--- bench/standard_load_tests.pl
+++ bench/standard_load_tests.pl
@@ -118,13 +118,12 @@ run_benchmark(  module => 'Krang::Story',
 ##############################################################
 my $krang_publish = catfile(KrangRoot, 'bin', 'krang_publish');
 $i = 0;
-
+my $publisher = Krang::Publisher->new;
 run_benchmark(  module => 'Krang::Story',
                 name => 'Publish 100 stories, one at a time',
                 count => 100,
                 code => sub {
-                    my $sid = $found_stories[$i++]->story_id;
-                   `$krang_publish --story_id $sid`;
+                    $publisher->publish_story(story => $found_stories[$i++]);
                 } ); 
 ##############################################################
 
