3819750ab51fefa7892aa82ef2963428552abd9e
jesse <jesse@689d98c0-9023-0410-9c0b-f79783857903>
2003-08-07 20:02:27 +0000

    Implemented update_db_version.  Prepped for test release
    git-svn-id: http://svn.krangcms.com/trunk/krang@1572 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang.pm
+++ lib/Krang.pm
@@ -2,7 +2,7 @@ package Krang;
 use strict;
 use warnings;
 
-our $VERSION = "0.012";
+our $VERSION = "0.013";
 
 =head1 NAME
 
---
+++ upgrade/V0_011.pm
@@ -0,0 +1,26 @@
+package V0_011;
+
+
+use Krang::Conf qw(InstanceDBName DBUser DBPass KrangRoot);
+use Krang::DB qw(dbh);
+
+
+# Nothing to do for this version....
+sub per_installation {
+    my $pkg = shift;
+
+    print STDERR "$pkg::per_installation:  KrangRoot => '". KrangRoot ."'\n";
+}
+
+
+# Create db_version table
+sub per_instance {
+    my $pkg = shift;
+
+    my $dbh = dbh();
+    my $instance = Krang::Conf->instance();
+    print STDERR "$pkg::per_instance:  instance => '$instance' ($dbh)\n";
+}
+
+
+1;
