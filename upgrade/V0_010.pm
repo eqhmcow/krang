4cd756a47e51190d89631f33b89a516f5268620f
jesse <jesse@689d98c0-9023-0410-9c0b-f79783857903>
2003-08-08 14:39:50 +0000

    Fixed bug in debugging output
    git-svn-id: http://svn.krangcms.com/trunk/krang@1578 689d98c0-9023-0410-9c0b-f79783857903

--- upgrade/V0_010.pm
+++ upgrade/V0_010.pm
@@ -9,19 +9,19 @@ use Krang::DB qw(dbh);
 
 # Nothing to do for this version....
 sub per_installation {
-    my $pkg = shift;
+    my $self = shift;
 
-    print STDERR "$pkg::per_installation:  KrangRoot => '". KrangRoot ."'\n";
+    print STDERR "$self\::per_installation:  KrangRoot => '". KrangRoot ."'\n";
 }
 
 
 # Create db_version table
 sub per_instance {
-    my $pkg = shift;
+    my $self = shift;
 
     my $dbh = dbh();
     my $instance = Krang::Conf->instance();
-    print STDERR "$pkg::per_instance:  instance => '$instance' ($dbh)\n";
+    print STDERR "$self\::per_instance:  instance => '$instance' ($dbh)\n";
 }
 
 
