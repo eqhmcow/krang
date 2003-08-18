443c20900bd308cbd7240ff393016c0e27325593
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2003-08-18 19:27:40 +0000

    Added mod_perl pre-loading for all Krang libraries (with a few exceptions) and all Krang UI templates.  This should speed up the UI and reduce memory usage.
    git-svn-id: http://svn.krangcms.com/trunk/krang@1603 689d98c0-9023-0410-9c0b-f79783857903

--- conf/krang-preload.pl
+++ conf/krang-preload.pl
@@ -4,11 +4,38 @@
 ####  MODULES TO PRE-LOAD INTO KRANG  ####
 ##########################################
 
-use Krang::ErrorHandler;
-use DBI;
-use Apache::DBI;
-use HTML::Template;
-use CGI;
+use Krang::Conf qw(KrangRoot);
+use File::Find qw(find);
+
+# load all Krang libs, with a few exceptions
+my $skip = qr/Profiler|Test|BricLoader|Cache|FTP|DataSet|Upgrade|MethodMaker|Daemon|Script/;
+find({ 
+      wanted => sub {
+          return unless m!(Krang/.*).pm$!;
+          my $path = $1;
+          return if /#/; # skip emacs droppings
+          return if /$skip/;
+
+          my $pkg = join('::', (split(/\//, $path)));
+          eval "use $pkg;";
+          die "Problem loading $pkg:\n\n$@" if $@;
+      },
+      no_chdir => 1
+     },
+     KrangRoot . '/lib/Krang');
+
+# load all template
+print STDERR "Pre-loading HTML Templates...\n";
+find(
+     sub {
+         return unless /\.tmpl$/;
+         HTML::Template->new(
+                             filename => "$File::Find::dir/$_",
+                             cache => 1,
+                             loop_context_vars => 1,
+                            );
+     },
+     KrangRoot . '/templates');
 
 
 print STDERR "Krang Pre-load complete.\n";
