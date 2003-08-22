3667b92f75097eede10c9f9165e168170702d82d
pleonard <pleonard@689d98c0-9023-0410-9c0b-f79783857903>
2003-08-22 18:22:20 +0000

    updates to Krang::DataSet benchmarks
    git-svn-id: http://svn.krangcms.com/trunk/krang@1650 689d98c0-9023-0410-9c0b-f79783857903

--- bench/dataset_load.pl
+++ bench/dataset_load.pl
@@ -10,55 +10,111 @@ use Krang::Benchmark qw(run_benchmark);
 use Krang::Conf qw(KrangRoot);
 use Krang::Script;
 use Krang::DataSet;
+use Krang::Site;
+
+my $site;
+
+($site) = Krang::Site->find ( limit => 1 );
+
+die "Unable to find site - exiting.\n" unless ($site);
+
+my $krang_export = catfile(KrangRoot, 'bin', 'krang_export');
+my $test_kds     = catfile(KrangRoot, 'bench', 'dataset_test_data.kds');
 
 
-# Note - this benchmark suite assumes that the desired data is already exported.
 
-my $story_kds = catfile(KrangRoot, 'export.kds');
+my $story_count = Krang::Story->find( count => 1 );
+my $media_count = Krang::Media->find( count => 1 );
+my $category_count = Krang::Category->find( count => 1 );
+my $contrib_count = Krang::Contrib->find( count => 1 );
 
-unless (-e $story_kds) {
-    die "Error:  Need a working .kds file at '$story_kds'.\n";
+my $totals = sprintf("%i st, %i med, %i cats, %i cntrb", $story_count, $media_count, $category_count, $contrib_count);
+
+############################################################
+## Export
+##
+
+my $done = 0;
+run_benchmark( module => 'Krang::DataSet',
+               name   => "Export everything ($totals)",
+               count  => $story_count,
+               code   => sub {
+                   if (not $done) {
+                       `$krang_export --output $test_kds --overwrite --everything`;
+                       $done = 1;
+                   }
+               }
+             );
+
+
+############################################################
+## new()
+##
+
+unless (-e $test_kds) {
+    die "Error:  Need a working .kds file at '$test_kds'.\n";
 }
 
+
 my $dataset;
-my %count;
-my @objects;
+$done = 0;
 
 run_benchmark(module => 'Krang::Dataset',
-              name   => 'new',
-              count  => 1,
+              name   => "new ($totals)",
+              count  => $story_count,
               code   =>
               sub {
-                  $dataset = Krang::DataSet->new(path => $story_kds);
+                  if (not $done) {
+                      $dataset = Krang::DataSet->new(path => $test_kds);
+                      $done = 1;
+                  }
               }
              );
 
 
+############################################################
+## list()
+
+$done = 0;
+my @objects;
+my %count;
+
 run_benchmark(module => 'Krang::Dataset',
-              name   => 'list',
-              count  => 1,
+              name   => "list ($totals)",
+              count  => $story_count,
               code =>
               sub {
-                  @objects = $dataset->list();
+                  if (not $done) {
+                      @objects = $dataset->list();
+                      $done = 1;
+                  }
               }
              );
 
 map { $count{$_->[0]}++ } @objects;
-
 use Data::Dumper;
-
 print Dumper(\%count);
 
+
+############################################################
+## import()
+
+$done = 0;
 run_benchmark(module => 'Krang::Dataset',
-              name   => 'import_all',
-              count  => 1,
+              name   => "import_all ($totals)",
+              count  => $story_count,
               code =>
               sub {
-                  @objects = $dataset->import_all();
+                  if (not $done) {
+                      @objects = $dataset->import_all();
+                      $done = 1;
+                  }
               }
              );
 
 
 
+
+unlink $test_kds;
 
 
