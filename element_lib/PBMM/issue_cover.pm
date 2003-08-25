e483e5c61c99a4825a03d594ca8f173369bc1cc1
mvella <mvella@689d98c0-9023-0410-9c0b-f79783857903>
2003-08-25 21:12:22 +0000

    more PBMM elements
    git-svn-id: http://svn.krangcms.com/trunk/krang@1662 689d98c0-9023-0410-9c0b-f79783857903

---
+++ element_lib/PBMM/issue_cover.pm
@@ -0,0 +1,57 @@
+package PBMM::issue_cover;
+use strict;
+use warnings;
+use PBMM::meta;
+
+use base 'Krang::ElementClass::Cover';
+
+sub new {
+   my $pkg = shift;
+   my @fixed = (min  => 1,
+                 max  => 1,
+                 reorderable => 0,
+                 allow_delete => 0);
+   my %args = ( name => 'issue_cover',
+                children => [
+                    PBMM::meta->new(),
+                    Krang::ElementClass::Text->new(name => 'issue_id',
+                                       max  => 1),
+                    Krang::ElementClass::CheckBox->new(name => 'enhanced_content',
+                                           @fixed),
+                    Krang::ElementClass::Text->new(name => 'promo_title',
+                                       @fixed),
+                    Krang::ElementClass::Textarea->new(name => 'promo_teaser',
+                                           @fixed),
+                    Krang::ElementClass::MediaLink->new(name => 'promo_image_small',
+                                            max  => 1),
+                    Krang::ElementClass::MediaLink->new(name => 'promo_image_large',
+                                            max  => 1),
+                    Default::lead_in->new(),
+                    PBMM::image->new(),
+                    Krang::ElementClass::Text->new(name         => "small_header" ),
+                    Krang::ElementClass::Text->new(name         => "large_header" ),   
+                    Krang::ElementClass::Textarea->new(name => "paragraph",
+                                                    bulk_edit => 1,
+                                                   ),
+                    PBMM::double_cover_column->new(   name => "double_column" ), 
+
+                ],
+                @_);
+   return $pkg->SUPER::new(%args);
+}
+
+# setup cover to republish hourly by default
+sub default_schedules {
+    my ($self, %arg) = @_;
+    my ($element) = @arg{qw(element)};
+    my $story = $element->story;
+    my $sched = Krang::Schedule->new(object_type => 'story',
+                                     object_id   => $story->story_id,
+                                     action      => 'publish',
+                                     repeat      => 'hourly',
+                                     minute      => 0);
+    croak("Unable to create schedule!") unless $sched;
+    return ($sched);
+}
+
+1;
