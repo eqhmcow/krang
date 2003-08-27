5805a53d24e608b881af79a4971f94c4d1e0e7e3
mvella <mvella@689d98c0-9023-0410-9c0b-f79783857903>
2003-08-27 18:50:42 +0000

    adding some category stuff, initial templates(most not finished yet)
    git-svn-id: http://svn.krangcms.com/trunk/krang@1677 689d98c0-9023-0410-9c0b-f79783857903

---
+++ element_lib/PBMM/category.pm
@@ -0,0 +1,59 @@
+package PBMM::category;
+use strict;
+use warnings;
+
+=head1 NAME
+
+PBMM::category
+
+=head1 DESCRIPTION
+
+PBMM category element class for Krang.  
+
+=cut
+
+
+use base 'Krang::ElementClass::TopLevel';
+
+sub new {
+   my $pkg = shift;
+   my %args = ( name => 'category',
+                children => [
+                    Krang::ElementClass::Text->new(name => 'display_name',
+                                                            allow_delete => 0,
+                                                            min => 1,
+                                                            max => 1,
+                                                            reorderable => 0,
+                                                            required => 1),
+                    Krang::ElementClass::Text->new(name => 'primary_css',
+                                                            max => 1,
+                                                            ),
+                    Krang::ElementClass::Text->new(name => 'secondary_css',
+                                                            max => 1,
+                                                            ),
+                    PBMM::top_cat_column->new(),
+                    PBMM::left_cat_column->new(),
+                    PBMM::right_cat_column->new(),
+                ],
+                @_);
+   return $pkg->SUPER::new(%args);
+}
+
+sub fill_template {
+    my ($self, %args) = @_;
+    my $story   = $args{publisher}->story;
+    my $tmpl      = $args{tmpl};
+    $tmpl->param( title =>  $story->title );
+    $tmpl->param( meta_description =>  $story->element->child('meta_description')->data );
+    my $keywords = $story->element->child('meta_keywords')->data;
+    my @keys;
+    foreach my $kw (@$keywords) {
+        push (@keys, {meta_keyword => $kw});
+    }
+    $tmpl->param( meta_keyword_loop => \@keys );
+    $self->SUPER::fill_template( %args ); 
+}
+
+1;
+
+   
---
+++ element_lib/PBMM/top_cat_column.pm
@@ -0,0 +1,59 @@
+package PBMM::top_cat_column;
+use strict;
+use warnings;
+
+=head1 NAME
+
+PBMM::top_cat_column
+
+=head1 DESCRIPTION
+
+PBMM top category column element class for Krang. 
+
+=cut
+
+
+use base 'Krang::ElementClass';
+
+sub new {
+   my $pkg = shift;
+
+   my @fixed = (min  => 1,
+                 max  => 1,
+                 reorderable => 0,
+                 allow_delete => 0);
+ 
+   my %args = ( name => 'top_cat_column',
+                min  => 1,
+                max => 1,
+                reorderable => 0,
+                children => 
+                [
+                 Krang::ElementClass::PopupMenu->new(name => "search_type",
+                                                     @fixed,
+                                                     values => [ "keyword",
+                                                                 "topic",
+                                                                 "multisite",
+                                                                 "custom"],
+                                                     default => "keyword"), 
+                Krang::ElementClass::MediaLink->new(name => "navigation_include"),
+                 Krang::ElementClass::Text->new(name         => "ad_module",
+                                                min => 1,
+                                                allow_delete => 0),
+                 Krang::ElementClass::MediaLink->new(name => "html_include",
+                                                     ),
+                 Krang::ElementClass::Text->new(name         => "small_header" ),
+                 Krang::ElementClass::Text->new(name         => "large_header" ),
+                 Krang::ElementClass::Textarea->new(name => "paragraph",
+                                                    required => 1,
+                                                    bulk_edit => 1,
+                                                   ),
+                 Default::lead_in->new(),
+                 PBMM::image->new(),
+                ],
+                @_);
+   return $pkg->SUPER::new(%args);
+}
+
+
+1;
