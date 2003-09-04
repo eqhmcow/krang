20ca6556f00693b17228dc65d701d1aae5053eda
mvella <mvella@689d98c0-9023-0410-9c0b-f79783857903>
2003-09-04 20:28:54 +0000

    this works now
    git-svn-id: http://svn.krangcms.com/trunk/krang@1724 689d98c0-9023-0410-9c0b-f79783857903

--- element_lib/PBMM/site_related_link_box.pm
+++ element_lib/PBMM/site_related_link_box.pm
@@ -46,5 +46,53 @@ sub new {
    return $pkg->SUPER::new(%args);
 }
 
+sub fill_template {
+    my ($self, %args) = @_;
+                                                                                
+    my $tmpl      = $args{tmpl};
+    my $cat   = $args{element}->object;
+    my $story   = $args{publisher}->story;
+   
+    my $keywords = $story->element->child('meta_keywords')->data;
+ 
+    # note the following searches thru all articles for matching meta
+    # keywords. in the future this should be indexed, but fix in 
+    # Krang::ElementClass is needed first.
+
+    my $top_cat = $cat;
+                                                                                
+    # get true top level category for site
+    while ( $top_cat->parent ) {
+        $top_cat = $top_cat->parent;
+    }
+
+    my @stories = Krang::Story->find( site_id => $top_cat->site->site_id, class => 'article', published => 1 ); 
+
+    my @article_loop;
+
+    foreach my $s (@stories) {
+        my $matched = 0;
+
+        next if ($s->story_id eq $story->story_id);
+        foreach my $key (@$keywords) {
+            my $ks = $s->element->child('meta_keywords')->data;
+            foreach my $k (@$ks) {
+                if ($k eq $key) {
+                    my %ps;
+                    $ps{title} = $s->element->child('promo_title')->data || $s->title;
+                    $ps{url} = $s->url;
+                    $ps{teaser} = $s->element->child('promo_teaser')->data if $s->element->child('promo_teaser')->data;
+                    push (@article_loop, \%ps );
+                    $matched = 1;
+                    last;
+                }
+            }
+            last if $matched;
+        }
+    }
+
+    $tmpl->param(article_loop => \@article_loop);
+    $self->SUPER::fill_template( %args );
+}
 
 1;
