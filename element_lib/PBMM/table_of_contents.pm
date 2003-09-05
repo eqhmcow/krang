2238697dcb03ae73e0ebd18473bab4a0e4c09cbd
mvella <mvella@689d98c0-9023-0410-9c0b-f79783857903>
2003-09-05 23:56:37 +0000

    table of contents now works, update of templates from alpha
    git-svn-id: http://svn.krangcms.com/trunk/krang@1751 689d98c0-9023-0410-9c0b-f79783857903

--- element_lib/PBMM/table_of_contents.pm
+++ element_lib/PBMM/table_of_contents.pm
@@ -37,5 +37,43 @@ sub new {
    return $pkg->SUPER::new(%args);
 }
 
+sub fill_template {
+    my ($self, %args) = @_;
+                                                                                
+    my $tmpl      = $args{tmpl};
+    my $cat   = $args{element}->object;
+    my $story   = $args{publisher}->story;
+    my $publisher = $args{publisher};
+
+    # if this is an issue cover, dont show it on side
+    return if ($story->class->name eq 'issue_cover');
+    
+    my $issue_id = $story->element->child('issue_id')->data || '';
+
+    return if (not $issue_id);
+    
+    my ($issue_cover) = Krang::Story->find( class => 'issue_cover', element_index => [ issue_id => $issue_id ]);
+
+    return if (not $issue_cover);
+
+    my $element = $issue_cover->element;
+    my @element_children = $element->children();
+    my @element_loop;
+
+    foreach my $child (@element_children) {
+        my $name     = $child->name;
+        if ($name eq 'lead_in') {
+            my $s = $child->child('story')->data;
+            my $ptitle = $s->element->child('promo_title')->data || $story->title;
+            my $pteaser = $s->element->child('promo_teaser')->data || '';
+            my $url = $child->child('story')->template_data(publisher => $publisher);
+            push(@element_loop, { title => $ptitle, teaser => $pteaser, url => $url, is_lead_in => 1 });
+        } elsif ($name eq 'large_header') {
+            push(@element_loop, { large_header => $child->publish( publisher => $publisher), "is_large_header" => 1 });    
+        }
+    }
+
+    $tmpl->param( element_loop => \@element_loop ) if @element_loop;
+}
 
 1;
