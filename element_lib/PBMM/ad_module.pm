a43e4484fbf384286df03fab69bb90ae3acd39c5
mvella <mvella@689d98c0-9023-0410-9c0b-f79783857903>
2003-09-16 23:02:11 +0000

    new element, ad module
    git-svn-id: http://svn.krangcms.com/trunk/krang@1797 689d98c0-9023-0410-9c0b-f79783857903

---
+++ element_lib/PBMM/ad_module.pm
@@ -0,0 +1,80 @@
+package PBMM::ad_module;
+use strict;
+use warnings;
+
+=head1 NAME
+
+PBMM::ad_module
+
+=head1 DESCRIPTION
+
+PBMM ad_module element class for Krang.
+Allows users to add an ad module of chosen type, and
+populated ad-related varibles based on current story and category.
+
+=cut
+
+
+use base 'Krang::ElementClass';
+
+sub new {
+   my $pkg = shift;
+   my %args = ( name => 'ad_module',
+                children => [
+                    Krang::ElementClass::PopupMenu->new(name => "size",
+                                                    reorderable => '0',
+                                                   min => 1,
+                                                   max => 1,
+                                                   allow_delete => '0',
+                                                   values => [  "468x60",
+                                                                "125x125",
+                                                                "120x600",
+                                                                "728x90"    ],
+                                                   default => "468x60"
+                                                           ),
+                ],
+                @_);
+   return $pkg->SUPER::new(%args);
+}
+
+sub input_form {
+    return '';
+}
+
+
+sub fill_template {
+    my ($self, %args) = @_;
+                                                                                
+    my $tmpl      = $args{tmpl};
+    my $cat   = $args{element}->object;
+    my $story   = $args{publisher}->story;
+   
+    $tmpl->param( slug => $story->slug );
+ 
+    my $parent = $args{element}->parent();
+    
+    $tmpl->param( "is_".$parent->name => 1);
+    
+    $tmpl->param( site => $cat->site()->url );
+    
+    if ($cat->parent) {
+        if ($cat->parent->parent) {
+            if ($cat->parent->parent->parent) {
+                $tmpl->param( cdir => $cat->parent->parent->dir );
+                $tmpl->param( scdir => $cat->parent->dir );
+                $tmpl->param( sscdir => $cat->dir );
+
+            } else {
+                $tmpl->param( cdir => $cat->parent->dir );
+                $tmpl->param( scdir => $cat->dir );
+            }      
+        } else {
+            $tmpl->param( cdir => $cat->dir );
+        }
+    } else {
+        $tmpl->param( cdir => 'home' ); 
+    }
+
+}
+
+1;
