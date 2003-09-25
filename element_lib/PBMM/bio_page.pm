1c083456db133e3bde97a5239526f921645b5454
mvella <mvella@689d98c0-9023-0410-9c0b-f79783857903>
2003-09-25 18:58:59 +0000

    added some fields
    git-svn-id: http://svn.krangcms.com/trunk/krang@1816 689d98c0-9023-0410-9c0b-f79783857903

--- element_lib/PBMM/bio_page.pm
+++ element_lib/PBMM/bio_page.pm
@@ -26,7 +26,11 @@ sub new {
                  Krang::ElementClass::Text->new(name => "small_header"),
                  Krang::ElementClass::Textarea->new(name => "paragraph",
                                                     bulk_edit => 1),
-                 PBMM::image->new(),
+                 Krang::ElementClass::Text->new(name => "name", max => 1),
+                 Krang::ElementClass::Text->new(name => "title", max => 1),
+                 Krang::ElementClass::Text->new(name => "email", max => 1),
+                 PBMM::image->new( max => 1 ),
+                 PBMM::image->new( name => 'logo', max => 1 ),
                  'document',
                 ],
                 @_);
