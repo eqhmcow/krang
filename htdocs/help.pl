3165635e1e8a00c504283daa1dff782cea231ee8
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2003-10-21 19:52:17 +0000

    Added Krang::CGI::Help to serve help pages for Krang.
    git-svn-id: http://svn.krangcms.com/trunk/krang@1888 689d98c0-9023-0410-9c0b-f79783857903

---
+++ htdocs/help.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl -w
+use Krang::ErrorHandler;
+use Krang::CGI::Help;
+Krang::CGI::Help->new()->run();
---
+++ lib/Krang/CGI/Help.pm
@@ -0,0 +1,57 @@
+package Krang::CGI::Help;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Krang::CGI::Help - Krang help screens
+
+=head1 SYNOPSIS
+
+  http://krang/help?topic=workspace
+
+=head1 DESCRIPTION
+
+Provides help screens given a topic.
+
+=head1 INTERFACE
+
+None.
+
+=cut
+
+use base 'Krang::CGI';
+use Krang::Conf qw(KrangRoot);
+use File::Spec::Functions qw(catfile catdir);
+use Krang::HTMLTemplate;
+
+sub setup {
+    my $self = shift;
+    $self->start_mode('show');
+    $self->run_modes(show => \&show);
+}
+
+sub show {
+    my $self = shift;
+    my $query = $self->query;
+    my $topic = $query->param('topic') or die "Missing required topic.";
+    die "Bad topic." if $topic !~ /^\w+/;
+
+    # find topic help file
+    my $file = catfile(KrangRoot, 'htdocs', 'help', "$topic.html");
+    if (not -e $file) { 
+        return "<h2>Unable to find help file for '$topic' topic.</h2>";
+    }
+
+    # load as template to process includes
+    my $template = Krang::HTMLTemplate->new(filename => $file,
+                                            path     => catdir(KrangRoot, 
+                                                               'templates', 
+                                                               'Help'),
+                                            search_path_on_includes => 1,
+                                            cache    => 1);
+
+    return $template->output;
+}
+
+1;
