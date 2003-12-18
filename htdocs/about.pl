6ae3c07b1c028f8359da24e1cbd75b43a88c79a7
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2003-12-18 21:34:45 +0000

    Added 'about Krang' screen showing version of Krang and installed addons.
    git-svn-id: http://svn.krangcms.com/trunk/krang@2094 689d98c0-9023-0410-9c0b-f79783857903

---
+++ htdocs/about.pl
@@ -0,0 +1,3 @@
+#!/usr/bin/perl -w
+use Krang::CGI::About;
+Krang::CGI::About->new()->run();
---
+++ lib/Krang/CGI/About.pm
@@ -0,0 +1,65 @@
+package Krang::CGI::About;
+use strict;
+use warnings;
+
+use base 'Krang::CGI';
+use Krang::Conf qw(KrangRoot);
+use File::Spec::Functions qw(catfile);
+use Krang;
+use Krang::AddOn;
+
+=head1 NAME
+
+Krang::CGI::About - show the About Krang screen
+
+=head1 SYNOPSIS
+
+  use Krang::CGI::About;
+  Krang::CGI::About->new()->run();
+
+=head1 DESCRIPTION
+
+Shows the About Krang screen listing version numbers and credits.
+
+=head1 INTERFACE
+
+=head2 RUN MODES
+
+=over
+
+=cut
+
+sub setup {
+    my $self = shift;
+    $self->mode_param('rm');
+    $self->start_mode('show');    
+    $self->run_modes(show => 'show');
+    $self->tmpl_path('About/');
+}
+
+=item show
+
+The only available run-mode, displays the about screen.
+
+=cut
+
+sub show {
+    my $self = shift;
+    my $template = $self->load_tmpl('about.tmpl');
+
+    $template->param(version => $Krang::VERSION);
+    
+    my @addons = sort { $a->name cmp $b->name } Krang::AddOn->find();
+    $template->param(addons => [ map { { name => $_->name,
+                                         version => $_->version } } @addons ])
+      if @addons;
+    
+
+    return $template->output();
+}
+
+=back
+
+=cut
+
+1;
