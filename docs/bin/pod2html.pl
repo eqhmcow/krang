8e843eb0532c7340b58641d6237c5b6da2d7fc27
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2004-03-24 16:26:42 +0000

    Added code to skip building HTML docs if source file hasn't changed since last build.  This makes 'make docs' much faster.
    git-svn-id: http://svn.krangcms.com/trunk/krang@2324 689d98c0-9023-0410-9c0b-f79783857903

--- docs/bin/pod2html.pl
+++ docs/bin/pod2html.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl -w
 $in = shift;
 $out = shift;
+if (-e $out and -e $in and (stat($out))[9] >= ((stat($in))[9])) {
+    print "Unchanged, skipping...\n";
+    exit(0);
+}
 system("bin/items.pl < $in > tmp.pod");
 system("pod2html --css docs.css --noindex tmp.pod " .
        "| bin/href.pl " .
