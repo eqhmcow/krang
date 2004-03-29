942bf95813f1e0600f2bef8ee738862451b791ab
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2004-03-29 13:27:23 +0000

    Added check for broken 5.8.0 install on Redhat 9.
    git-svn-id: http://svn.krangcms.com/trunk/krang@2354 689d98c0-9023-0410-9c0b-f79783857903

--- platform/Redhat9/Platform.pm
+++ platform/Redhat9/Platform.pm
@@ -13,4 +13,25 @@ sub guess_platform {
     return 0;
 }
 
+sub verify_dependencies {
+    my ($pkg, %arg) = @_;
+
+    # if this is Perl 5.8.0 then we need to check that the locale
+    # isn't set to something UTF8-ish since that breaks this perl
+    my $perl = join('.', (map { ord($_) } split("", $^V, 3)));
+    if ($perl eq '5.8.0' and $ENV{LANG} =~ /UTF-8/) {
+        die <<END;
+
+Your version of Perl (v5.8.0) must not be used with a UTF-8 locale
+setting.  You can fix this problem by either upgrading Perl to v5.8.3
+or later or by editing /etc/sysconfig/i18n and choosing a non-UTF-8
+LANG setting (ex: en_US).
+
+END
+
+    }
+
+    return $pkg->SUPER::verify_dependencies(%arg);
+}
+
 1;
