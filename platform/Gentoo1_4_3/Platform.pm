17036eb4976b104520c202e76f177cc9080f3d00
pleonard <pleonard@689d98c0-9023-0410-9c0b-f79783857903>
2004-04-02 11:34:26 +0000

    platform/
    git-svn-id: http://svn.krangcms.com/trunk/krang@2400 689d98c0-9023-0410-9c0b-f79783857903

--- platform/Gentoo1_4_3/Platform.pm
+++ platform/Gentoo1_4_3/Platform.pm
@@ -4,6 +4,8 @@ use warnings;
 
 use base 'Krang::Platform';
 
+use Cwd qw(cwd);
+
 sub guess_platform {
     return 0 unless -e '/etc/gentoo-release';
     open(RELEASE, '/etc/gentoo-release') or return 0;
@@ -27,6 +29,28 @@ sub verify_dependencies {
     }
 
     return $pkg->SUPER::verify_dependencies(%arg);
+}
+
+
+# setup init script in /etc/init.d.
+# Being Gentoo, it will be up to the users to make it start on boot.
+
+sub finish_installation {
+    my ($pkg, %arg) = @_;
+    my %options = %{$arg{options}};
+
+    my $init_script = "krang-". $options{HostName};
+    print "Installing Krang init.d script '$init_script'\n";
+
+    my $old = cwd;
+    chdir("/etc/init.d");
+
+    my $InstallPath = $options{InstallPath};
+    unlink $init_script if -e $init_script;
+    my $link_init = "ln -s $InstallPath/bin/krang_ctl $init_script";
+    system($link_init) && die ("Can't link init script: $!");
+
+    chdir $old;
 }
 
 1;
