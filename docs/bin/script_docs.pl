bceaace5eb04ddae82bda50f1550e8270220654f
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2004-05-03 11:46:41 +0000

    Added code to generate HTML documentation for the scripts in bin/.
    git-svn-id: http://svn.krangcms.com/trunk/krang@2516 689d98c0-9023-0410-9c0b-f79783857903

---
+++ docs/bin/script_docs.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w
+use strict;
+
+open POD, ">scripts.pod" or die $!;
+print POD <<END;
+=head1 Krang Scripts
+
+END
+
+opendir(DIR, "../bin") or die "Can't open ../bin: $!";
+my @scripts = sort grep { /^krang_/ } readdir(DIR);
+
+foreach my $script (@scripts) {
+    return if $script =~ /#/; # ignore emacs droppings
+
+    # get short desc line from POD
+    my $desc = "";
+    open(PM, "../bin/$script") or die "Can't open ../bin/$script: $!";
+    while(my $line = <PM>) {
+        next unless $line =~ /^=head1 NAME/;        
+        last;
+    }
+    while(my $line = <PM>) {
+        next if $line =~ /^\s*$/;
+        ($desc) = $line =~ /\-\s+(.*)$/;
+        last;
+    }
+    close(PM);
+            
+    my $fname = "script_$script";
+
+    # reference in modules.html
+    print POD "HREF[$script|$fname.html] ". ($desc ? " - $desc" : "") . "\n\n";
+
+    # make um
+    print "bin/pod2html.pl ../bin/$script $fname.html\n";
+    system("bin/pod2html.pl ../bin/$script $fname.html");
+}
+
+close POD;
+
+
+
