3c640350c50a7eb4daf012e55772cbcb27761754
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2004-06-03 13:01:49 +0000

    Fixed the v1.018 upgrade module to successfully upgrade instances from v1.016 and older.  Also made changes to allow repeated runs of the upgrade module to succeed even if the upgrade stops part way through.
    git-svn-id: http://svn.krangcms.com/trunk/krang@2583 689d98c0-9023-0410-9c0b-f79783857903

--- upgrade/V1_018.pm
+++ upgrade/V1_018.pm
@@ -13,30 +13,38 @@ sub per_instance {
 
     my $dbh  = dbh();
 
-    # add the new hidden column to 'user'
-    $dbh->do("ALTER TABLE user ADD COLUMN hidden BOOL");
-    $dbh->do("ALTER TABLE user ALTER COLUMN hidden SET DEFAULT 0");
-    
     # rebuild permissions cache, moved here from V1_017 because it
     # will fail with the current Krang::User before user.hidden is
     # added
     print "Rebuilding permissions cache...\n";
+
+    # load Krang::Script to get REMOTE_USER setup which is needed by
+    # Krang::Group
+    local $ENV{KRANG_INSTANCE} = Krang::Conf->instance();
     eval 'use Krang::Script';
     Krang::Group->rebuild_category_cache();
 
-    # add 'system' user
-    my ($group_id) = Krang::Group->find(name => 'Admin', ids_only => 1);
-    my $user = Krang::User->new(login      => 'system',
-                                email      => 'system@noemail.com',
-                                first_name => 'System',
-                                last_name  => 'User',
-                                password   => '*',
-                                encrypted  => 1,
-                                hidden     => 1,
-                               );
-    $user->group_ids($group_id);
+    # add the new hidden column to 'user' unless it already exists
+    my @rows = $dbh->selectrow_array("SHOW COLUMNS FROM user LIKE 'hidden'");
+    unless (@rows) {
+        $dbh->do("ALTER TABLE user ADD COLUMN hidden BOOL");
+        $dbh->do("ALTER TABLE user ALTER COLUMN hidden SET DEFAULT 0");
+    }
 
-    $user->save();
+    # add 'system' user if one does not exists
+    unless (Krang::User->find(login => 'system', count => 1)) {
+        my ($group_id) = Krang::Group->find(name => 'Admin', ids_only => 1);
+        my $user = Krang::User->new(login      => 'system',
+                                    email      => 'system@noemail.com',
+                                    first_name => 'System',
+                                    last_name  => 'User',
+                                    password   => '*',
+                                    encrypted  => 1,
+                                    hidden     => 1,
+                                   );
+        $user->group_ids($group_id);
+        $user->save();
+    }
 }
 
 # nothing yet
