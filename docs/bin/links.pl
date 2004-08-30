b3819f9141a91be3e1f01a6515a0bc4e57d8fe49
pleonard <pleonard@689d98c0-9023-0410-9c0b-f79783857903>
2004-08-30 00:20:57 +0000

    minor regexp fix - should be case-insensitive
    git-svn-id: http://svn.krangcms.com/trunk/krang@2693 689d98c0-9023-0410-9c0b-f79783857903

--- docs/bin/links.pl
+++ docs/bin/links.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/perl -p
 
 # fixup links to modules
-s!<A[^>]+>\s*the\s+(\S+)\s+manpage\s*</A>!find_link($1)!ge;
+s!<A[^>]+>\s*the\s+(\S+)\s+manpage\s*</A>!find_link($1)!ige;
 
 sub find_link {
     my $mod = shift;
+
     if ($mod =~ /^Krang::/) {
         my $fname = lc($mod);
         $fname =~ s/::/_/g;
