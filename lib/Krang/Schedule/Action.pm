8b9b35604d5dca761de015248220ce818a4aa48f
adancygier <adancygier@689d98c0-9023-0410-9c0b-f79783857903>
2005-06-27 19:02:37 +0000

    new method contains code common to all action class execute methods.
    git-svn-id: http://svn.krangcms.com/trunk/krang@3026 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/Schedule/Action.pm
+++ lib/Krang/Schedule/Action.pm
@@ -30,6 +30,8 @@ and the derived class must at a minimum implement method sub execute().
 
 =item sub execute() must be redefined in derived class.
 
+=back
+
 =cut
 
 sub execute {
@@ -37,6 +39,27 @@ sub execute {
     my $msg = sprintf("%s->execute(): unknown action '%s'", __PACKAGE__, $self->{action});
     $msg .= "\nmust define execute() in subclass of Schedule::Action\n";
         die($msg);
+}
+
+=over
+
+=item sub clean_entry() method for book keeping after executing job. 
+
+=back
+
+=cut
+
+sub clean_entry {
+    my $self = shift;
+
+    if ($self->{repeat} eq 'never') {
+        # never to be run again.  delete yourself.
+        $self->delete();
+    } else {         # set last_run, update next_run, save.
+        $self->{last_run} = $self->{next_run};
+        $self->{next_run} = $self->_calc_next_run(skip_match => 1);
+        $self->save();
+    }
 }
 
 =back
