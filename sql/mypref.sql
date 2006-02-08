52243d646fc6c4fff2fa52a620592ee760700297
bodo <bodo@689d98c0-9023-0410-9c0b-f79783857903>
2006-02-08 23:41:43 +0000

    Use user_id and id as primary key so that users don't clobber each others preferences.
    git-svn-id: http://svn.krangcms.com/trunk/krang@3234 689d98c0-9023-0410-9c0b-f79783857903

--- sql/mypref.sql
+++ sql/mypref.sql
@@ -1,8 +1,8 @@
 /* my_pref table holds data managed by Krang::MyPref */
 DROP TABLE IF EXISTS my_pref;
 CREATE TABLE my_pref (
-        id      VARCHAR(255) NOT NULL PRIMARY KEY,
+        id      VARCHAR(255) NOT NULL,
         user_id int(10) unsigned NOT NULL,
-        value   VARCHAR(255) NOT NULL
+        value   VARCHAR(255) NOT NULL,
+        PRIMARY KEY (user_id, id)	
 );
-
