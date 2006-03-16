30e30eac000098f9f594048d74c6ba1c27ee2761
mpeters <mpeters@689d98c0-9023-0410-9c0b-f79783857903>
2006-03-16 16:58:07 +0000

    removing unneede files
    git-svn-id: http://svn.krangcms.com/trunk/krang@3290 689d98c0-9023-0410-9c0b-f79783857903

--- htdocs/element_editor.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl -w
-use Krang::ClassFactory qw(pkg);
-use Krang::ClassLoader 'ErrorHandler';
-use Krang::ClassLoader 'CGI::ElementEditor';
-my $app = pkg('CGI::ElementEditor')->new();
-$app->run();
--- htdocs/nav.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl -w
-use Krang::ClassFactory qw(pkg);
-use Krang::ClassLoader 'CGI::Nav';
-pkg('CGI::Nav')->new()->run();
