286a856038eaed8fce9dbc57416698aa917d0e04
jerlbaum <jerlbaum@689d98c0-9023-0410-9c0b-f79783857903>
2006-07-25 21:25:20 +0000

    krang_upload_templates script now handles global templates
    git-svn-id: http://svn.krangcms.com/trunk/krang@3369 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/Template.pm
+++ lib/Krang/Template.pm
@@ -742,7 +742,7 @@ sub find {
             my $tmp = join(" OR ", map {"t.template_id = ?"} @{$args{$arg}});
             $where_clause .= " ($tmp)";
             push @params, @{$args{$arg}};
-        } elsif ($arg eq 'category_id' && ref ($args{$arg}) eq 'ARRAY') {
+        } elsif ($arg eq 'category_id' && (ref($args{$arg}) || "") eq 'ARRAY') {
             my $tmp= join(" OR ", map {"t.category_id = ?"} @{$args{$arg}});
             $where_clause .= " ($tmp)";
             push @params, @{$args{$arg}};
