81af36e3943c7514b02eb742c9821946384a0fb2
mpeters <mpeters@689d98c0-9023-0410-9c0b-f79783857903>
2006-08-31 19:12:24 +0000

    use the browser helper methods instead of matching the browser_string. This makes it easy to avoid warnings about uninitialized values since some browsers (Konqueror) don't have a browser_string() value.
    git-svn-id: http://svn.krangcms.com/trunk/krang@3402 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/Handler.pm
+++ lib/Krang/Handler.pm
@@ -226,14 +226,14 @@ sub access_handler ($$) {
     # is it Netscape 6+, IE 5+ (but not mac), or Mozilla/Firefox?
     my $bd = HTTP::BrowserDetect->new($r->header_in('User-Agent'));
     if (
-        ($bd->browser_string eq 'Netscape' and $bd->major >= 5) or
-        ($bd->browser_string eq 'MSIE'     and $bd->major >= 5 and !$bd->mac) or
-        ($bd->browser_string eq 'Mozilla'  and $bd->major >= 1) or
-        ($bd->browser_string eq 'Firefox'  and (($bd->major >= 1) or
+        ($bd->netscape  and $bd->major >= 5) or
+        ($bd->ie        and $bd->major >= 5 and !$bd->mac) or
+        ($bd->mozilla   and $bd->major >= 1) or
+        ($bd->firefox   and (($bd->major >= 1) or
                                                 ($bd->minor == .1 or
                                                  $bd->minor >= .8))
         ) or
-        ($bd->browser_string eq 'Safari' and $bd->major >= 1) or
+        ($bd->safari    and $bd->major >= 1) or
         ($bd->konqueror and $bd->major >= 1)
        ) {
         return OK;
