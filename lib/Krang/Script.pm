a50217dd5b40ef67506618030c0a2f286b721ef9
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2006-11-06 21:55:11 +0000

    Modified Krang::Script to optionally skip su-ing to KrangUser.  This is useful in krang_export to avoid chicken-and-egg problems with Dataset, which needs to be loaded before su-ing but also needs access to addons.
    git-svn-id: http://svn.krangcms.com/trunk/krang@3459 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/Script.pm
+++ lib/Krang/Script.pm
@@ -8,17 +8,24 @@ use warnings;
 Krang::Script - loader for the Krang scripts
 
 =head1 SYNOPSIS
- 
+
+  # load normally
   use Krang::ClassLoader 'Script';
+
+  # load without switching priviliges
+  use Krang::ClassLoader Script => 'no_su';
 
 =head1 DESCRIPTION
 
 This module exists to load and configure the Krang system for
 command-line scripts.
 
-The first thing the module does is attempt to become the configured
-KrangUser and KrangGroup.  If you're not already KrangUser then you'll
-need to be root in order to change into KrangUser.
+The first thing the module does is call any registered addon
+InitHandlers.  See F<add_on.pod> for details.
+
+Next Krang attempts to become the configured KrangUser and KrangGroup,
+unless passed the 'no_su' option.  If you're not already KrangUser
+then you'll need to be root in order to change into KrangUser.
 
 Next the module sets REMOTE_USER to the user ID of the special hidden
 'system' user.  This user has global admin access to all Krang
@@ -49,49 +56,51 @@ use Carp qw(croak);
 use Krang::ClassLoader 'User';
 use Krang::ClassLoader 'AddOn';
 
-# trigger init handler now
-BEGIN { pkg('AddOn')->call_handler('InitHandler') }
+sub import {
+    my $pkg = shift;
+    my %opts = map { ($_, 1) } @_;
 
-BEGIN {
+    # trigger init handler now
+    pkg('AddOn')->call_handler('InitHandler');
+    
     # make sure we are KrangUser/KrangGroup
+    unless ($opts{'no_su'}) {
+        # get current uid/gid
+        my $uid = $>;
+        my %gid = map { ($_ => 1) } split( ' ', $) );
+        
+        # extract desired uid/gid
+        my @uid_data = getpwnam(KrangUser);
+        warn("Unable to find user for KrangUser '" . KrangUser . "'."), exit(1)
+          unless @uid_data;
+        my $krang_uid = $uid_data[2];
+        my @gid_data = getgrnam(KrangGroup);
+        warn("Unable to find user for KrangGroup '" . KrangGroup . "'."), exit(1)
+          unless @gid_data;
+        my $krang_gid = $gid_data[2];
+        
+        # become KrangUser/KrangGroup if necessary
+        if ($gid{$krang_gid}) {
+            eval { $) = $krang_gid; };
+            warn("Unable to become KrangGroup '" . KrangGroup . "' : $@\n" . 
+                 "Maybe you need to start this process as root.\n") and exit(1)
+                   if $@;
+            warn("Failed to become KrangGroup '" . KrangGroup . "' : $!.\n" .
+                 "Maybe you need to start this process as root.\n") and exit(1)
+                   unless $) == $krang_gid;
+        }
 
-    # get current uid/gid
-    my $uid = $>;
-    my %gid = map { ($_ => 1) } split( ' ', $) );
-
-    # extract desired uid/gid
-    my @uid_data = getpwnam(KrangUser);
-    warn("Unable to find user for KrangUser '" . KrangUser . "'."), exit(1)
-      unless @uid_data;
-    my $krang_uid = $uid_data[2];
-    my @gid_data = getgrnam(KrangGroup);
-    warn("Unable to find user for KrangGroup '" . KrangGroup . "'."), exit(1)
-      unless @gid_data;
-    my $krang_gid = $gid_data[2];
-
-    # become KrangUser/KrangGroup if necessary
-    if ($gid{$krang_gid}) {
-        eval { $) = $krang_gid; };
-        warn("Unable to become KrangGroup '" . KrangGroup . "' : $@\n" . 
-            "Maybe you need to start this process as root.\n") and exit(1)
-          if $@;
-        warn("Failed to become KrangGroup '" . KrangGroup . "' : $!.\n" .
-            "Maybe you need to start this process as root.\n") and exit(1)
-          unless $) == $krang_gid;
+        if ($uid != $krang_uid) {
+            eval { $> = $krang_uid; };
+            warn("Unable to become KrangUser '" . KrangUser . "' : $@\n" .
+                 "Maybe you need to start this process as root.\n") and exit(1)
+                   if $@;
+            warn("Failed to become KrangUser '" . KrangUser . "' : $!\n" .
+                 "Maybe you need to start this process as root.\n") and exit(1)
+                   unless $> == $krang_uid;
+        }
     }
 
-    if ($uid != $krang_uid) {
-        eval { $> = $krang_uid; };
-        warn("Unable to become KrangUser '" . KrangUser . "' : $@\n" .
-            "Maybe you need to start this process as root.\n") and exit(1)
-          if $@;
-        warn("Failed to become KrangUser '" . KrangUser . "' : $!\n" .
-             "Maybe you need to start this process as root.\n") and exit(1)
-          unless $> == $krang_uid;
-    }
-}
-
-BEGIN {
     # Set Krang instance if not running under mod_perl
     my $instance = $ENV{KRANG_INSTANCE};
     if (not defined $instance) {
