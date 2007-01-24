fcc7d178315fd0c8df8aa27af62fbd2d02e54c39
sam <sam@689d98c0-9023-0410-9c0b-f79783857903>
2007-01-24 18:16:53 +0000

    Narrowed test to allow .files again in bulk-upload.
    git-svn-id: http://svn.krangcms.com/trunk/krang@3526 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/CGI/Media/BulkUpload.pm
+++ lib/Krang/CGI/Media/BulkUpload.pm
@@ -387,10 +387,9 @@ sub build_image_list {
 
     # ignore fluff in MacOSX .zip files
     return if $path =~ /__MACOSX/;
+    return if $file =~ /^\.DS_Store$/;
+    return if $file =~ /^\.localized$/;
 
-    # ignore .files
-    return if $file =~ /^\./;
-   
     my $opened_root = $File::Find::topdir; 
     $path =~ s/$opened_root//; 
     $path =~ s/^\/// if $path;
