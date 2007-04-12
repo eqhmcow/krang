2e0f69776f596fc277975a377c9c8825fe118b11
bodo <bodo@689d98c0-9023-0410-9c0b-f79783857903>
2007-04-12 13:18:17 +0000

    Since first and middle name are concatenated with &nbsp;, we can't html-escape first_middle in the template.  So we'll do it one by one in Krang::CGI::Contrib.
    git-svn-id: http://svn.krangcms.com/trunk/krang@3742 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/CGI/Contrib.pm
+++ lib/Krang/CGI/Contrib.pm
@@ -896,10 +896,11 @@ sub delete {
 # Set up special row_handler for associate contrib screen
 sub list_view_ass_contrib_row_handler {
     my $self = shift;
+    my $q = $self->query;
     my ($row_hashref, $contrib, $associated_contrib_filter, $contrib_type_prefs) = @_;
 
-    $row_hashref->{first_middle} = $contrib->first;
-    $row_hashref->{first_middle} .= '&nbsp;' . $contrib->middle if ($contrib->middle);
+    $row_hashref->{first_middle} = $q->escapeHTML($contrib->first);
+    $row_hashref->{first_middle} .= '&nbsp;' . $q->escapeHTML($contrib->middle) if ($contrib->middle);
     $row_hashref->{last} = $contrib->last;
 
     # 'type' isn't actually used.  Loop 'contrib_types' instead
