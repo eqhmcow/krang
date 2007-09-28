484943291cc56aa4c417cda508e5e0acee06e21e
mpeters <mpeters@689d98c0-9023-0410-9c0b-f79783857903>
2007-09-28 21:34:31 +0000

    + Move mysql_charset() from bin/krang_charset_convert to Krang::Charset + bin/krang_createdb now honors Charset() directive when creating databases
    git-svn-id: http://svn.krangcms.com/trunk/krang@4125 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/Charset.pm
+++ lib/Krang/Charset.pm
@@ -17,6 +17,10 @@ Krang::Charset - some handy utility methods for dealing with character sets
     pkg('Charset')->is_utf8('UTF 8');       # true
     pkg('Charset')->is_utf8('utf-8');       # true
 
+    # what does mysql want?
+    pkg('Charset')->mysql_charset('UTF 8');         # utf8
+    pkg('Charset')->mysql_charset('ISO 8859-1');    # latin1
+
 =head1 DESCRIPTION
 
 This class just provides a collection of methods that are useful
@@ -26,7 +30,7 @@ when operating on character sets.
 
 =head2 C<< Krang::Charset->is_utf8([$charset]) >>
 
-Returns true if the character set looks like UTF-8.  Defaults to using
+Returns true if the character set looks like UTF-8. Defaults to using
 the configured characters set if none is given.
 
 =cut
@@ -47,5 +51,31 @@ sub is_utf8 {
     my ($class, $charset ) = @_;
     return ( $charset ? _munge_charset($charset) : $MUNGED ) eq 'utf8';
 }
+
+=head2 C<< Krang::Charset->mysql_charset([$charset])
+
+This method tries to convert the given charset into something that MySQL
+can understand. Defaults to using the configured characters set if none
+is given.
+
+=cut
+
+# try and convert it to something that mysql likes
+my %MYSQL_MAP = (
+    iso88591    => 'latin1',
+    iso88592    => 'latin2',
+    iso88598    => 'hebrew',
+    iso88599    => 'latin5',
+    cp1252      => 'latin1',
+    windows1252 => 'latin1',
+);
+sub mysql_charset {
+    my ($pkg, $charset) = @_;
+    $charset = $charset ? _munge_charset($charset) : $MUNGED;
+
+    return $MYSQL_MAP{$charset} || $charset;
+}
+
+
 
 1;
