65edbc6ef140aed722eec3ea95961dce7150dfb4
jesse <jesse@689d98c0-9023-0410-9c0b-f79783857903>
2007-11-21 01:27:27 +0000

    Merged from v3 release branch, up to SVN r4354
    git-svn-id: http://svn.krangcms.com/trunk/krang@4355 689d98c0-9023-0410-9c0b-f79783857903

--- upgrade/V3_00.pm
+++ upgrade/V3_00.pm
@@ -88,65 +88,71 @@ sub _wipe_slugs_from_cover_stories {
 # now that the URLs of categories and stories-with-slugs must be distinct,
 # look for duplicates and - when found - resolve them by creating new categories
 sub _fix_slugs_that_duplicate_categories {
-					  
-  print "\nLooking for Stories that must be turned into Covers to avoid URL conflicts: ";
-  
-  # grab the ID of every story that conflicts with a category
-  my $dbh = dbh();
-  my $query = 'select sc.story_id from category c, story_category sc where c.url=concat(sc.url, "/")';
-  my $result = $dbh->selectall_arrayref($query, undef);
-  if ($result && @$result) {
-    print "Found " . @$result . "\n\n";
-    
-    # loop through stories, converting them into category covers
-    foreach (@$result) {
-      my $story_id = $_->[0];
-      print "Converting Story $story_id into a Category Cover...\n";
-      
-      # grab story object and - if necessary - check it in
-      my ($story) = Krang::Story->find(story_id => $story_id);
-      if ($story->checked_out) {
-	print "First checking it in: ";
-	eval { $story->checkin; };
-	if ($@) {
-	  print " FAILED (skipping)\n\n";
-	}
-	print "Done\n";
-	    
-	# give story temporary slug so we don't throw dupe error during conversion!
-	$story->checkout;
-	my $slug = $story->slug;
-	$story->slug('_TEMP_SLUG_FOR_CONVERSION_'); $story->save;
+    print "\nLooking for Stories that must be turned into Covers to avoid URL conflicts: ";
 
-        # make new categories by appending slug to existing categories
-        my @old_cats = $story->categories;
-        my @new_cats;
-        foreach my $old_cat (@old_cats) {
-  	  my ($new_cat) = Krang::Category->find(url => $old_cat->url . $slug . '/');
-          unless ($new_cat) {
-            print "Creating new category: ". $old_cat->url . $slug ."/\n";
-            $new_cat = Krang::Category->new(dir       => $slug,
-                                            parent_id => $old_cat->category_id,
-                                            site_id   => $old_cat->site_id);
-            $new_cat->save;
-          } else {
-	    print "Found existing category: ".$new_cat->url."\n";
-          }
-          push @new_cats, $new_cat;
-          print "Moving story from " . $old_cat->url . " to " . $new_cat->url . "\n";
-        }
-        print "Emptying slug.\n";
-	$story->slug('');
+    # grab the ID of every story that conflicts with a category
+    my $dbh = dbh();
+    my $query = 'select sc.story_id from category c, story_category sc where c.url=concat(sc.url, "/")';
+    my $result = $dbh->selectall_arrayref($query, undef);
+
+    if ($result && @$result) {
+        print "Found " . @$result . "\n\n";
+    
+        # Set REMOTE_USER -- needed for API calls
+        local($ENV{REMOTE_USER}) = 2; # Magic ID of "system" user
+  
+        # loop through stories, converting them into category covers
+        foreach (@$result) {
+            my $story_id = $_->[0];
+
+            # grab story object and - if necessary - check it in
+            my ($story) = Krang::Story->find(story_id => $story_id);
+
+            print "  Converting Story $story_id (".$story->url.") into a Category Cover...\n";
+
+            # Force check-in, if necessary
+            if ($story->checked_out) {
+                print "    First checking it in: ";
+                eval { $story->checkin; };
+                if ($@) {
+                    print " FAILED (skipping)\n\n";
+                }
+                print " Done\n";
+            }
 	    
-	# save changes
-        $story->categories(@new_cats);
-        $story->save; 
-	$story->checkin;
-      }
+            # give story temporary slug so we don't throw dupe error during conversion!
+            $story->checkout;
+            my $slug = $story->slug;
+            $story->slug('_TEMP_SLUG_FOR_CONVERSION_'); $story->save;
+
+            # make new categories by appending slug to existing categories
+            my @old_cats = $story->categories;
+            my @new_cats;
+            foreach my $old_cat (@old_cats) {
+                my ($new_cat) = Krang::Category->find(url => $old_cat->url . $slug . '/');
+                unless ($new_cat) {
+                    print "    Creating new category: ". $old_cat->url . $slug ."/\n";
+                    $new_cat = Krang::Category->new(dir       => $slug,
+                                                    parent_id => $old_cat->category_id,
+                                                    site_id   => $old_cat->site_id);
+                    $new_cat->save;
+                } else {
+                    print "    Found existing category: ".$new_cat->url."\n";
+                }
+                push @new_cats, $new_cat;
+                print "    Moving story from " . $old_cat->url . " to " . $new_cat->url . "\n";
+            }
+            print "    Emptying slug.\n";
+            $story->slug('');
+	    
+            # save changes
+            $story->categories(@new_cats);
+            $story->save; 
+            $story->checkin;
+        }
+    } else {
+        print "None found.\n\n";
     }
-  } else {
-    print "None found.\n\n";
-  }
 }
 
 1;
