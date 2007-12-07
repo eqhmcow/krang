c3fef275d8d466b4ea9cff7330c555331d4f5ae6
bodo <bodo@689d98c0-9023-0410-9c0b-f79783857903>
2007-12-07 11:11:21 +0000

    Modified Krang::Message to provide access to the localized message databases in lang/messages.LANGUAGE.
    git-svn-id: http://svn.krangcms.com/trunk/krang@4477 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/Message.pm
+++ lib/Krang/Message.pm
@@ -3,7 +3,7 @@ use Krang::ClassFactory qw(pkg);
 use strict;
 use warnings;
 
-use Krang::ClassLoader Conf => qw(KrangRoot);
+use Krang::ClassLoader Conf => qw(KrangRoot AvailableLanguages);
 use Krang::ClassLoader Session => qw(%session);
 use File::Spec::Functions qw(catfile);
 use Carp qw(croak);
@@ -11,10 +11,13 @@ use Krang::ClassLoader Log => qw(debug);
 use Krang::ClassLoader 'File';
 use Krang::ClassLoader 'Charset';
 use Encode qw(decode_utf8 encode_utf8);
+use I18N::LangTags qw(is_language_tag);
 use Class::ISA;
 
 use base 'Exporter';
 our @EXPORT_OK = qw(add_message get_messages get_message_text clear_messages add_alert get_alerts clear_alerts);
+
+our %CONF;
 
 =head1 NAME
 
@@ -40,7 +43,7 @@ Krang::Message - module to handle UI messages in Krang
   # get messages in a hash by key
   %messages = get_messages(keys => 1);
 
-  # get the text of a message as defined in the the F<messages.conf> file.
+  # get the text of a message as defined in the the F<lang/messages.LANGUAGE> file.
   $text = get_message_text('invalid_title', 'Krang::CGI::Story');
 
   # clear messages
@@ -53,11 +56,13 @@ These allows you to register a message or alert which will be shown to
 the user at the next convenient point.  In the web UI that will be on
 the next screen shown. 
 
-Messages are specified symbolically, allowing Krang to maintain a
-centralized database of message text in F<conf/messages.conf>.  This
-allows non-programmers to edit this text.  Variable replacement in
-error text is specified using a Perlish syntax.  For example, if the
-C<duplicate_url> message were specified like this:
+Messages are specified symbolically, allowing Krang to maintain
+centralized language-specific databases of message text in
+F<lang/messages.LANGUAGE>, with LANGUAGE being a RFC3066-style
+language tag.  This allows non-programmers to edit this text.
+Variable replacement in error text is specified using a Perlish
+syntax.  For example, if the C<duplicate_url> message were specified
+like this:
 
   duplicate_url "Duplicate!  The story with ID $id already has the URL $url."
 
@@ -65,7 +70,7 @@ Then the call to C<add_message()> should look like:
 
   add_message('duplicate_url', id => $id, url => $url);
 
-Messages in F<conf/messages.conf> may be specified in per-module
+Messages in F<lang/messages.LANGUAGE> may be specified in per-module
 blocks.  For example, Krang::CGI::Story's messages could be specified
 like:
 
@@ -76,13 +81,13 @@ like:
 
 This allows different modules to define the same name differently.
 
-Addons can include their own F<conf/messages.conf> file which will be
-read in after the standard Krang file, overriding any duplicated
+Addons can include their own F<lang/messages.LANGUAGE> file which will
+be read in after the standard Krang file, overriding any duplicated
 entries.
 
-Any messages found in F<conf/messages.conf> can used by either C<add_message()>
-or C<add_alert()>. The method chosen will effect how the message is displayed to
-the user.
+Any messages found in F<lang/messages.LANGUAGE> can be used by either
+C<add_message()> or C<add_alert()>. The method chosen will effect how
+the message is displayed to the user.
 
 =head1 INTERFACE
 
@@ -96,7 +101,7 @@ the user.
 
 Adds a message to the current list of messages.  The first parameter
 is always the message identifier, which must appear in
-F<conf/messages.conf>.  It must either be in the block for the current
+F<lang/messages.LANGUAGE>.  It must either be in the block for the current
 module or outside of any block.  Any parameters after the message name
 are parameters to the message, except for C<_from_module> which sets
 an alternate module name to use to lookup the message definition.
@@ -108,7 +113,6 @@ name.
 =cut
 
 sub add_message {
-    our ($CONF);
     my ($key, %args) = @_;
     my @caller = caller;
     my $from_module = delete $args{from_module} || $caller[0];
@@ -116,7 +120,7 @@ sub add_message {
 
     # get the text of this message with any variable substitution
     my $message = get_message_text($key, $from_module, %args);
-    croak("Unable to find message '$key' in conf/messages.conf ".
+    croak("Unable to find message '$key' in lang/messages.$session{language} ".
           "for '$from_module'")
       unless $message;
 
@@ -134,7 +138,7 @@ sub add_message {
 
 Adds an alert to the current list of messages.  The first parameter
 is always the message identifier, which must appear in
-F<conf/messages.conf>. It must either be in the block for the current
+F<lang/messages.LANGUAGE>. It must either be in the block for the current
 module or outside of any block.  Any parameters after the alert name
 are parameters to the alert, except for C<from_module> which sets
 an alternate module name to use to lookup the message definition.
@@ -146,7 +150,6 @@ name.
 =cut
 
 sub add_alert {
-    our ($CONF);
     my ($key, %args) = @_;
     my @caller = caller;
     my $from_module = delete $args{from_module} || $caller[0];
@@ -154,7 +157,7 @@ sub add_alert {
 
     # get the text of this alert including variable substitution
     my $alert = get_message_text($key, $from_module, %args);
-    croak("Unable to find alert '$key' in conf/messages.conf ".
+    croak("Unable to find alert '$key' in lang/messages.$session{language} ".
           "for '$from_module'")
       unless $alert;
 
@@ -178,7 +181,7 @@ returns a hash of messages keyed by message identifier.
 sub get_messages {
     my %args = (dups => 0, keys => 0, @_);
     my $messages = $session{messages} || [];
-    
+
     # return key=>message mapping
     return map { @$_ } @$messages
       if $args{keys};
@@ -208,7 +211,7 @@ returns a hash of alerts keyed by alert identifier.
 sub get_alerts {
     my %args = (dups => 0, keys => 0, @_);
     my $alerts = $session{alerts} || [];
-    
+
     # return key=>alert mapping
     return map { @$_ } @$alerts
       if $args{keys};
@@ -231,8 +234,12 @@ they can be provided by using passing in extra C<%args>.
 =cut
 
 sub get_message_text {
-    our ($CONF);
     my ($key, $class, %args) = @_;
+
+    my $language = $session{language} || 'en';
+
+    my $CONF = $CONF{$language};
+
     my $msg;
     foreach my $module (Class::ISA::self_and_super_path($class)) {
         # get handle for the module block, if there is one
@@ -253,7 +260,7 @@ sub get_message_text {
     while (my ($name, $value) = each %args) {
         unless ($msg =~ s/\$\Q$name\E/$value/g) {
             croak("Unable to find substitution variable '$name' for message ".
-                  "'$key' in conf/messages.conf");
+                  "'$key' in lang/messages.$language");
         }
     }
 
@@ -283,14 +290,28 @@ sub clear_alerts {
     $session{alerts} = [];
 }
 
-# load the configuration file
+# load the configuration files
 sub _load_config {
-    # find all messages.conf files in reverse order to give the
-    # intended overriding effect
-    my @files = reverse Krang::File->find_all("conf/messages.conf");
-    
-    our $CONF = Config::ApacheFormat->new();
-    $CONF->read($_) for @files;
+    our %CONF;
+
+    # load at least the English messages file
+    my @languages = AvailableLanguages;
+    @languages = ('en') unless scalar(@languages);
+
+    # load them for all AvailableLanguages or just English
+    for my $lang (@languages) {
+	croak "$lang is not a valid language tag"
+	  unless is_language_tag($lang);
+
+	# find all lang/messages.LANGUAGE files in reverse order to
+	# give the intended overriding effect
+	my @files = reverse Krang::File->find_all(catfile('lang', "messages.$lang"));
+
+	my $conf = Config::ApacheFormat->new();
+	$conf->read($_) for @files;
+
+	$CONF{$lang} = $conf;
+    }
 }
 BEGIN { _load_config() };
 
