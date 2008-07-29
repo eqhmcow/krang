36dc40f7938adfc28d8b34fa7998b3f927e876bc
noam <noam@689d98c0-9023-0410-9c0b-f79783857903>
2008-07-29 22:20:05 +0000

    prevent back-door approach to adding/deleting contributors without admin_contribs permissions
    git-svn-id: http://svn.krangcms.com/trunk/krang@5252 689d98c0-9023-0410-9c0b-f79783857903

--- htdocs/contributor.pl
+++ htdocs/contributor.pl
@@ -3,5 +3,14 @@ use Krang::ClassFactory qw(pkg);
 
 use Krang::ClassLoader 'CGI::Contrib';
 my $app = pkg('CGI::Contrib')->new(
-    PARAMS => {},
+    PARAMS => {
+        RUNMODE_PERMISSIONS => {
+            add            => [ 'admin_contribs' ],
+            save_add       => [ 'admin_contribs' ],
+            save_stay_add  => [ 'admin_contribs' ],
+            save_edit      => [ 'admin_contribs' ],
+            save_stay_edit => [ 'admin_contribs' ],
+            delete         => [ 'admin_contribs' ]
+        }
+    }
 )->run();
