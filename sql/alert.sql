33c8326fe1e659db9ecfe2355fc6fd3588ed28b4
noam <noam@689d98c0-9023-0410-9c0b-f79783857903>
2008-09-18 21:18:41 +0000

    added missing entries to alert.sql - previously in upgrade but not new install
    git-svn-id: http://svn.krangcms.com/trunk/krang@5401 689d98c0-9023-0410-9c0b-f79783857903

--- sql/alert.sql
+++ sql/alert.sql
@@ -5,13 +5,16 @@ DROP TABLE IF EXISTS alert;
 --
 
 CREATE TABLE alert (
-    alert_id int(10) unsigned NOT NULL auto_increment,
+    alert_id int(10) unsigned not NULL auto_increment,
     user_id int(10) unsigned not NULL,
     action varchar(255) default NULL,
     desk_id int(10) unsigned default NULL,
     category_id int(10) unsigned default NULL,
     object_type varchar(255) default NULL,
     object_id int unsigned default NULL,
-    PRIMARY KEY (alert_id)
+    custom_msg_subject VARCHAR(255) default NULL,
+    custom_msg_body TEXT default NULL,
+    PRIMARY KEY (alert_id),
+    KEY `object_type` (`object_type`,`object_id`)
 ) TYPE=MyISAM;
 
