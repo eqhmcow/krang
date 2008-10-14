457e6e42b3359ccc63d168c9788bb26c3c9c1747
noam <noam@689d98c0-9023-0410-9c0b-f79783857903>
2008-10-14 22:29:33 +0000

    added warning to v3.04 upgrade script about newly-created files (and ran perltidy on whole script)
    git-svn-id: http://svn.krangcms.com/trunk/krang@5490 689d98c0-9023-0410-9c0b-f79783857903

--- upgrade/V3_04.pm
+++ upgrade/V3_04.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Krang::ClassFactory qw(pkg);
 use Krang::ClassLoader base => 'Upgrade';
-use Krang::ClassLoader DB => 'dbh';
+use Krang::ClassLoader DB   => 'dbh';
 use Krang::ClassLoader 'ElementLibrary';
 use Krang::Conf qw(KrangRoot InstanceElementSet);
 use File::Spec::Functions qw(catfile catdir);
@@ -25,35 +25,39 @@ sub per_instance {
         $dbh->do('REPLACE INTO pref (id, value) VALUES ("language", "en")');
         print "DONE\n\n";
     }
-    
+
     # 2. add the new schedule options
-    my @schedule_columns = @{$dbh->selectcol_arrayref('SHOW columns FROM schedule')};
-    my %new_schedule_cols = ('failure_max_tries' => 'int unsigned', 
-                             'failure_delay_sec' => 'int unsigned',
-                             'failure_notify_id' => 'int unsigned', 
-                             'success_notify_id' => 'int unsigned');
+    my @schedule_columns  = @{$dbh->selectcol_arrayref('SHOW columns FROM schedule')};
+    my %new_schedule_cols = (
+        'failure_max_tries' => 'int unsigned',
+        'failure_delay_sec' => 'int unsigned',
+        'failure_notify_id' => 'int unsigned',
+        'success_notify_id' => 'int unsigned'
+    );
     foreach my $new_col (keys %new_schedule_cols) {
         print "Adding '$new_col' column to schedule table... ";
         if (grep { $_ eq $new_col } @schedule_columns) {
             print "already exists (skipping)\n\n";
         } else {
-            $dbh->do("ALTER TABLE schedule ADD $new_col ".$new_schedule_cols{$new_col});
+            $dbh->do("ALTER TABLE schedule ADD $new_col " . $new_schedule_cols{$new_col});
             print "DONE\n\n";
         }
     }
 
     # 3. add the new alert columns
     my @alert_columns  = @{$dbh->selectcol_arrayref('SHOW columns FROM alert')};
-    my %new_alert_cols = (object_type        => 'varchar(255)', 
-                          object_id          => 'int unsigned',
-                          custom_msg_subject => 'varchar(255)', 
-                          custom_msg_body    => 'text');
+    my %new_alert_cols = (
+        object_type        => 'varchar(255)',
+        object_id          => 'int unsigned',
+        custom_msg_subject => 'varchar(255)',
+        custom_msg_body    => 'text'
+    );
     foreach my $new_col (keys %new_alert_cols) {
         print "Adding '$new_col' column to alert table... ";
         if (grep { $_ eq $new_col } @alert_columns) {
             print "already exists (skipping)\n\n";
         } else {
-            $dbh->do("ALTER TABLE alert ADD $new_col ".$new_alert_cols{$new_col});
+            $dbh->do("ALTER TABLE alert ADD $new_col " . $new_alert_cols{$new_col});
             print "DONE\n\n";
         }
     }
@@ -72,8 +76,8 @@ sub per_instance {
     $self->add_elements_to_media();
 }
 
-
 sub add_elements_to_media {
+    my ($element_lib, $warn_about_new_pm, $warn_about_set_conf);
 
     # 1. add column to media table
     my $dbh = dbh();
@@ -85,7 +89,7 @@ sub add_elements_to_media {
         $dbh->do('ALTER TABLE media ADD element_id int unsigned NOT NULL');
         print "DONE\n\n";
     }
-    
+
     # 2. check for media without elements
     print "Checking for media objects without element_ids... ";
     my $media_ids = $dbh->prepare('SELECT media_id FROM media WHERE element_id = 0');
@@ -96,7 +100,7 @@ sub add_elements_to_media {
     } else {
         print "none found (skipping)\n\n";
     }
-    
+
     # 3. determine class name for media element
     my $class = 'media';
     pkg('ElementLibrary')->load_set(set => InstanceElementSet);
@@ -118,13 +122,13 @@ sub add_elements_to_media {
 
     # unless the class has already been built...
     unless (pkg('ElementLibrary')->find_class(name => $class)) {
-    
+
         # 4. locate element library
-        my $set = InstanceElementSet;
+        my $set     = InstanceElementSet;
         my $package = $set . '::' . $class;
-        my $element_lib;
         foreach (catdir(KrangRoot, 'addons', $set, 'element_lib', $set),
-                 catdir(KrangRoot, 'element_lib', $set)) {
+            catdir(KrangRoot, 'element_lib', $set))
+        {
             print "\nLooking for element library in $_... ";
             if (-d $_) {
                 $element_lib = $_;
@@ -134,9 +138,9 @@ sub add_elements_to_media {
         if ($element_lib) {
             print "found\n\n";
         } else {
-            croak ("failed - don't know where else to look!\n\n");
+            croak("failed - don't know where else to look!\n\n");
         }
-        
+
         # 5. create skeletal media element
         my $path_to_module;
         my $filename = 'media';
@@ -147,7 +151,7 @@ sub add_elements_to_media {
         }
         print "Creating $path_to_module... ";
         open MODULE, " > $path_to_module";
-        print MODULE <<"EOF"; 
+        print MODULE <<"EOF";
 package $package;
 
 =head1 NAME
@@ -176,6 +180,7 @@ sub new {
 1;
 EOF
         print "done\n\n";
+        $warn_about_new_pm = 1;
 
         # 6. add media element to set.conf
         print "Adding an entry to $element_lib/set.conf... ";
@@ -184,49 +189,64 @@ EOF
         close SET_CONF;
         my $lines = join(@lines);
         if ($lines =~ /\s$class\s/) {
-           print "already exists (skipping)\n\n";
-        } else {  
-           open SET_CONF, " > $element_lib/set.conf";
-           foreach (@lines) {
-             if ($_ =~ /TopLevels (.*)/i) {
-               print SET_CONF "TopLevels $class $1\n";
-             } else {
-               print SET_CONF "$_";
-             }
-           }
-           close SET_CONF;
-           print "done\n";
+            print "already exists (skipping)\n\n";
+        } else {
+            open SET_CONF, " > $element_lib/set.conf";
+            foreach (@lines) {
+                if ($_ =~ /TopLevels (.*)/i) {
+                    print SET_CONF "TopLevels $class $1\n";
+                } else {
+                    print SET_CONF "$_";
+                }
+            }
+            close SET_CONF;
+            print "done\n";
+            $warn_about_set_conf = 1;
         }
-  }
+    }
 
-  # unless all media objects already have element ids...
-  if ($media_without_elements_total) {
+    # unless all media objects already have element ids...
+    if ($media_without_elements_total) {
 
-      # 7. add rows to element and media tables
-      print "\nAdding root element to $media_without_elements_total media rows... \r";
-      my $add_element = $dbh->prepare(qq{
+        # 7. add rows to element and media tables
+        print "\nAdding root element to $media_without_elements_total media rows... \r";
+        my $add_element = $dbh->prepare(
+            qq{
          INSERT INTO element (class) values ('$class')
-      });
+      }
+        );
 
-      my $multi_table_update = $dbh->prepare(q{
+        my $multi_table_update = $dbh->prepare(
+            q{
         UPDATE media, element SET
           media.element_id = element.element_id,
           element.root_id = element.element_id
         WHERE media.media_id = ? AND element.element_id = ?
-      });
-
-      my $written;
-      while (my ($media_id) = $media_ids->fetchrow_array()) {
-         $add_element->execute;
-         my $element_id = $dbh->{mysql_insertid} || die 'No mysql_insert_id';
-         $multi_table_update->execute($media_id, $element_id);
-         print "Adding root element to $media_without_elements_total media rows... $written written\r"
-            unless (++$written % 50);
       }
-      print "Adding root element to $media_without_elements_total media rows... done\n";
-  }
+        );
 
-  print "\n";
+        my $written;
+        while (my ($media_id) = $media_ids->fetchrow_array()) {
+            $add_element->execute;
+            my $element_id = $dbh->{mysql_insertid} || die 'No mysql_insert_id';
+            $multi_table_update->execute($media_id, $element_id);
+            print
+              "Adding root element to $media_without_elements_total media rows... $written written\r"
+              unless (++$written % 50);
+        }
+        print "Adding root element to $media_without_elements_total media rows... done\n";
+    }
+
+    if ($warn_about_new_pm) {
+        print
+          "\n* * * WARNING * * * \t The Krang upgrade script has created a new file: $element_lib/$class.pm\n";
+    }
+    if ($warn_about_set_conf) {
+        print
+          "\n* * * WARNING * * * \t The Krang upgrade script has added an entry for $class to $element_lib/set.conf\n";
+    }
+
+    print "\n";
 }
 
 1;
