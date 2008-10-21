48b7bbc1129537bbc3cd3baca7c712c52f6ceb16
mpeters <mpeters@689d98c0-9023-0410-9c0b-f79783857903>
2008-10-21 13:39:19 +0000

    fixed problem noticed by Noam where the disabled list-btn buttons that had submenus would still show their submenu when hovered
    git-svn-id: http://svn.krangcms.com/trunk/krang@5514 689d98c0-9023-0410-9c0b-f79783857903

--- htdocs/js/krang.js
+++ htdocs/js/krang.js
@@ -589,12 +589,15 @@ Krang.Form = {
         if (!ckbx) { ckbx = 'krang_pager_rows_checked' }
         [ [$('C'), 'list-btn'], [form, 'mini-list-btn'] ].each(function(spec) {
             spec[0].select('.'+spec[1]).each(function(btn) {
+             var submenu = btn.next('ul');
                 if (Krang.row_checked(form, ckbx)) {
                     btn.addClassName(spec[1]+'-enabled');
                     btn.enable();
+                    if(submenu) submenu.removeClassName('disabled');
                 } else {
                     btn.removeClassName(spec[1]+'-enabled');
                     btn.disable();
+                    if(submenu) submenu.addClassName('disabled');
                     spec[0].select('input#checkallbox').first().checked = false;
                 }
             })});
