a70e6fced6f85f354f2ad5bdd5e762676603e538
bodo <bodo@689d98c0-9023-0410-9c0b-f79783857903>
2008-11-03 17:30:45 +0000

    Removed the textNode sibling code from PoorText.getRangeContainerIndices() since it messed up the caret position.
    git-svn-id: http://svn.krangcms.com/trunk/krang@5588 689d98c0-9023-0410-9c0b-f79783857903

--- htdocs/poortext/poortext_w3c_range.js
+++ htdocs/poortext/poortext_w3c_range.js
@@ -11,8 +11,6 @@
                      given node relatively to its parentNode and
                      recursively up the node tree until the given
                      parent is reached.
-                     Key 'r' is an array of corresponding negative
-                     offsets counted from the end of the array
 */
 PoorText.getRangeContainerIndices = function(containerNode, rootNode) {
     var offsets  = [];
@@ -35,17 +33,21 @@ PoorText.getRangeContainerIndices = function(containerNode, rootNode) {
         containerNode = parentNode;
     }
 
+//
+// This does not work with the button-driven special-char-insertion
+//
+
     //normalized() can not be called so often to prevent
     //invalidating selection/range, so we have to detect
-    //here that any text nodes in a row
-    if(offsets.length > 0 && origNode.nodeType == 3){
-        n = origNode.previousSibling;
-
-        while(n && n.nodeType == 3){
-            offsets[offsets.length-1]--;
-            n = n.previousSibling;
-        }
-    }
+//    //here that any text nodes in a row
+//    if(offsets.length > 0 && origNode.nodeType == 3){
+//        n = origNode.previousSibling;
+//
+//        while(n && n.nodeType == 3){
+//            offsets[offsets.length-1]--;
+//            n = n.previousSibling;
+//        }
+//    }
 
     return offsets;
 }
@@ -57,7 +59,7 @@ PoorText.getRangeContainerIndices = function(containerNode, rootNode) {
    @param rootNode - DomNode serving as the starting point for drilling down
 */
 PoorText.getRangeContainerNode = function(indices, rootNode){
-    if(!Object.isArray(indices) || indices.length==0){
+    if(!Object.isArray(indices) || indices.length == 0){
         return rootNode;
     }
 
@@ -68,7 +70,7 @@ PoorText.getRangeContainerNode = function(indices, rootNode){
             node = node.childNodes[i];
         } else {
             node = null;
-            alert('Error: can not find node with index '+i+' under rootNode node '+rootNode );
+//            alert('Error: can not find node with index '+i+' under rootNode node '+rootNode );
             return false;
         }
     });
