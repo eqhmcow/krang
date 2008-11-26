e0fd745209c34936439fbfa95fa1e052f1de673f
noam <noam@689d98c0-9023-0410-9c0b-f79783857903>
2008-11-26 01:22:35 +0000

    Fixed bug in Xinha.pm where element classes that didn't override bulk_save_change were breaking class/HTML, causing ISE (Bodo, please verify this change, since I may have been doing something else wrong...)
    git-svn-id: http://svn.krangcms.com/trunk/krang@5677 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/BulkEdit/Xinha.pm
+++ lib/Krang/BulkEdit/Xinha.pm
@@ -301,7 +301,10 @@ sub add_element {
         my $class = $arg{elementclass_for}->{$tag} || $arg{elementclass_for}->{p};
 
         # maybe change the element's class
-        ($class, $html) = $element->bulk_save_change(class => $class, data => $html);
+        if (my ($new_class, $new_html) = $element->bulk_save_change(class => $class, data => $html)) {
+            $class = $new_class;
+            $html  = $new_html;
+        }
 
         # make a new Krang element for this tag's content
         my $child = $element->add_child(class => $class);
