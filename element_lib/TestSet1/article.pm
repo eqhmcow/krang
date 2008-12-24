c2c1816e18bbb5ddc849e134dbd439e027b8ffcd
bodo <bodo@689d98c0-9023-0410-9c0b-f79783857903>
2008-12-24 17:11:01 +0000

    Reformatted.
    git-svn-id: http://svn.krangcms.com/trunk/krang@5730 689d98c0-9023-0410-9c0b-f79783857903

--- element_lib/TestSet1/article.pm
+++ element_lib/TestSet1/article.pm
@@ -19,84 +19,105 @@ use Krang::ClassLoader base => 'ElementClass::TopLevel';
 use Krang::ElementClass::CheckBoxGroup;
 
 sub new {
-   my $pkg = shift;
-   my %args = ( name => 'article',
-                children => 
-                 [
-                  pkg('ElementClass::Date')->new(name => 'issue_date',
-                                                 min  => 1,
-                                                 max  => 1,
-                                                 reorderable => 0,
-                                                 allow_delete => 0),
-                  pkg('ElementClass::Textarea')->new(name => 'deck', 
-                                                     min => 1, 
-                                                     max => 1,
-                                                     reorderable => 0,
-                                                     allow_delete => 0,
-                                                     indexed => 1,
-                                                    ),
-                  pkg('ElementClass::CheckBoxGroup')->new( name => 'cbg_values',
-                                                           values => [map { "option\_$_" } ('a'..'z')],
-                                                           defaults => [qw(option_c option_e)],
-                                                           columns => 4,
-                                                           min => 1,
-                                                           max => 1,
-                                                           allow_delete => 1,
-                                                           reorderable => 1 ),
-                  pkg('ElementClass::ListGroup')->new( name => 'cbg_listgroup',
-                                                           list_group => "Cost",
-                                                       size=>1,
-                                                           min => 1,
-                                                           max => 1,
-                                                           allow_delete => 1,
-                                                           reorderable => 1 ),
-                  pkg('ElementClass::ListGroup')->new( name => 'cbg_listgroup_2',
-                                                           list_group => "Make/Model/Year",
-                                                           min => 1,
-                                                       size=>1,
-                                                           max => 1,
-                                                           allow_delete => 1,
-                                                           reorderable => 1 ),
-                  pkg('ElementClass::ListGroup')->new(  name => 'auto_segments',
-                                                        list_group => 'Segments',
-                                                        multiple => 1,
-                                                        min => 1,
-                                                        max => 1,
-                                                        allow_delete => 1,
-                                                        reorderable => 1 ),
-                  TestSet1::fancy_keyword->new(min          => 1,
-                                               max          => 1,
-                                               reorderable  => 0,
-                                               allow_delete => 0,
-                                               indexed => 1,
-                                              ),
-                  pkg('ElementClass::Textarea')->new(name => 'blurb',
-                                                     bulk_edit => 1),
+    my $pkg  = shift;
+    my %args = (
+        name     => 'article',
+        children => [
+            pkg('ElementClass::Date')->new(
+                name         => 'issue_date',
+                min          => 1,
+                max          => 1,
+                reorderable  => 0,
+                allow_delete => 0
+            ),
+            pkg('ElementClass::Textarea')->new(
+                name         => 'deck',
+                min          => 1,
+                max          => 1,
+                reorderable  => 0,
+                allow_delete => 0,
+                indexed      => 1,
+            ),
+            pkg('ElementClass::CheckBoxGroup')->new(
+                name         => 'cbg_values',
+                values       => [map { "option\_$_" } ('a' .. 'z')],
+                defaults     => [qw(option_c option_e)],
+                columns      => 4,
+                min          => 1,
+                max          => 1,
+                allow_delete => 1,
+                reorderable  => 1
+            ),
+            pkg('ElementClass::ListGroup')->new(
+                name         => 'cbg_listgroup',
+                list_group   => "Cost",
+                size         => 1,
+                min          => 1,
+                max          => 1,
+                allow_delete => 1,
+                reorderable  => 1
+            ),
+            pkg('ElementClass::ListGroup')->new(
+                name         => 'cbg_listgroup_2',
+                list_group   => "Make/Model/Year",
+                min          => 1,
+                size         => 1,
+                max          => 1,
+                allow_delete => 1,
+                reorderable  => 1
+            ),
+            pkg('ElementClass::ListGroup')->new(
+                name         => 'auto_segments',
+                list_group   => 'Segments',
+                multiple     => 1,
+                min          => 1,
+                max          => 1,
+                allow_delete => 1,
+                reorderable  => 1
+            ),
+            TestSet1::fancy_keyword->new(
+                min          => 1,
+                max          => 1,
+                reorderable  => 0,
+                allow_delete => 0,
+                indexed      => 1,
+            ),
+            pkg('ElementClass::Textarea')->new(
+                name      => 'blurb',
+                bulk_edit => 1
+            ),
 
-                  pkg('ElementClass::RadioGroup')->new(name => 'mood',
-                                                       values => [qw/happy sad confused manic depressive cynical/],
-                                                       labels => { happy      => "Happy",
-                                                                   sad        => "Sad",
-                                                                   confused   => "Confused",
-                                                                   manic      => "Manic",
-                                                                   depressive => "Depressive",
-                                                                   cynical    => "Cynical" },
-                                                       columns => 2 ),
+            pkg('ElementClass::RadioGroup')->new(
+                name   => 'mood',
+                values => [qw/happy sad confused manic depressive cynical/],
+                labels => {
+                    happy      => "Happy",
+                    sad        => "Sad",
+                    confused   => "Confused",
+                    manic      => "Manic",
+                    depressive => "Depressive",
+                    cynical    => "Cynical"
+                },
+                columns => 2
+            ),
 
-                  pkg('ElementClass::RadioGroup')->new(name => 'radio_cost',
-                                                       min          => 1,
-                                                       max          => 1,
-                                                       list_group => 'Cost',
-                                                       columns => 2 ),
+            pkg('ElementClass::RadioGroup')->new(
+                name       => 'radio_cost',
+                min        => 1,
+                max        => 1,
+                list_group => 'Cost',
+                columns    => 2
+            ),
 
-                  'page',
-                ],
-                @_);
-   return $pkg->SUPER::new(%args);
+            'page',
+        ],
+        @_
+    );
+    return $pkg->SUPER::new(%args);
 }
 
 # test delete_hook
 our $DELETE_COUNT = 0;
-sub delete_hook { $DELETE_COUNT++ };
+sub delete_hook { $DELETE_COUNT++ }
 
 1;
