18036be0be6e14d1c57dffc3ea083bed983a4c7e
mpeters <mpeters@689d98c0-9023-0410-9c0b-f79783857903>
2009-01-20 19:46:00 +0000

    we dont need both bug.cgi and bug.pl, so just use bug.cgi, but make it use the new V2 addon stuff that bug.pl used
    git-svn-id: http://svn.krangcms.com/trunk/krang@5769 689d98c0-9023-0410-9c0b-f79783857903

--- htdocs/bug.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl -w
-use Krang::ClassFactory qw(pkg);
-use Krang::ClassLoader 'ErrorHandler';
-use Krang::ClassLoader 'CGI::Bugzilla';
-my $app = pkg('CGI::Bugzilla')->new();
-$app->run();
