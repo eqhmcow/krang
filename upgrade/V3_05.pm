1c693ced35cbab7492a87c805bd98ce1efa39dd2
mpeters <mpeters@689d98c0-9023-0410-9c0b-f79783857903>
2009-01-29 15:32:46 +0000

    we need to actually import this as a routine to use it
    git-svn-id: http://svn.krangcms.com/trunk/krang@5821 689d98c0-9023-0410-9c0b-f79783857903

--- upgrade/V3_05.pm
+++ upgrade/V3_05.pm
@@ -4,7 +4,7 @@ use warnings;
 use Krang::ClassFactory qw(pkg);
 use Krang::ClassLoader base => 'Upgrade';
 
-use Krang::ClassLoader Conf => qw(KrangRoot);
+use Krang::ClassLoader Conf => qw(KrangRoot InstanceElementSet);
 use Krang::ClassLoader DB   => 'dbh';
 use Krang::ClassLoader 'ElementLibrary';
 use Krang::ClassLoader 'ListGroup';
