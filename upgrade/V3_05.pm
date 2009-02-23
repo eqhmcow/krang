9a74846ce77ecac948bcbfe1452b43973dd2fb24
mpeters <mpeters@689d98c0-9023-0410-9c0b-f79783857903>
2009-02-23 05:08:56 +0000

    use the instance name, not the element lib name since they arent the same thing
    git-svn-id: http://svn.krangcms.com/trunk/krang@5886 689d98c0-9023-0410-9c0b-f79783857903

--- upgrade/V3_05.pm
+++ upgrade/V3_05.pm
@@ -19,7 +19,8 @@ sub per_instance {
     my ($self, %args) = @_;
     return if $args{no_db};
     my $dbh = dbh();
-    print "\n\nUPGRADING INSTANCE " . uc(InstanceElementSet) . ":\n\n";
+    my $instance = pkg('Conf')->instance();
+    print "\n\nUPGRADING INSTANCE $instance:\n\n";
 
     # add the new published flag to media
     my @media_columns = @{$dbh->selectcol_arrayref('SHOW columns FROM media')};
