08aa4e3dd3b17e01728b77dc5a84de4df12d12b6
mpeters <mpeters@689d98c0-9023-0410-9c0b-f79783857903>
2009-04-23 22:13:43 +0000

    we dont need to initialize $html when declared since other calls can make it undef, so just make sure that undef isnt used in the concat
    git-svn-id: http://svn.krangcms.com/trunk/krang@6121 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/ElementClass.pm
+++ lib/Krang/ElementClass.pm
@@ -987,7 +987,7 @@ sub fill_template {
     foreach my $child (@element_children) {
         my $name       = $child->name;
         my $child_loop = $name . '_loop';
-        my $html = '';
+        my $html;
         my %fill_template_args;
 
         # skip this child unless something in the template references
@@ -1069,7 +1069,7 @@ sub fill_template {
                 publisher          => $publisher,
                 fill_template_args => \%fill_template_args
             );
-            $child_params{$name} = $div . $html;
+            $child_params{$name} = $div . ($html || '');
         }
     }
 
