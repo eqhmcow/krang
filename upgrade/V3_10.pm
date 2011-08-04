bae59ccdeb510fcce733d589df1694b412387048
bodo <bodo@689d98c0-9023-0410-9c0b-f79783857903>
2011-08-04 12:06:15 +0000

    Added upgrade script that adds a trailing slash to column story_category.url (required by patch in r6389
    git-svn-id: http://svn.krangcms.com/trunk/krang@6545 689d98c0-9023-0410-9c0b-f79783857903

---
+++ upgrade/V3_10.pm
@@ -0,0 +1,23 @@
+package V3_10;
+use strict;
+use warnings;
+use Krang::ClassFactory qw(pkg);
+use Krang::ClassLoader base => 'Upgrade';
+use Krang::ClassLoader Conf => qw(KrangRoot InstanceElementSet);
+use Krang::ClassLoader DB   => 'dbh';
+use Krang::ClassLoader 'ElementLibrary';
+use File::Spec::Functions qw(catfile);
+use Cwd qw(cwd);
+
+sub per_instance {
+    my ($self, %args) = @_;
+    return if $args{no_db};
+    my $dbh = dbh();
+    $dbh->do('UPDATE story_category set url = concat(url, "/")');
+}
+
+sub per_installation {
+    # nothing yet
+}
+
+1;
