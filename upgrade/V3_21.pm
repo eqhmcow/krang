4310576b32e910a1ff9be127659282aa93744d16
mpeters <mpeters@689d98c0-9023-0410-9c0b-f79783857903>
2012-07-06 15:04:35 +0000

    upgrade for 3.21 to add story_tag table
    git-svn-id: http://svn.krangcms.com/trunk/krang@6827 689d98c0-9023-0410-9c0b-f79783857903

---
+++ upgrade/V3_21.pm
@@ -0,0 +1,33 @@
+package V3_21;
+use strict;
+use warnings;
+use Krang::ClassFactory qw(pkg);
+use Krang::ClassLoader base => 'Upgrade';
+use Krang::ClassLoader DB   => 'dbh';
+use Krang::ClassLoader 'Media';
+
+sub per_instance {
+    my ($self, %args) = @_;
+    return if $args{no_db};
+    my $dbh = dbh();
+
+    # add the story_tag table
+    $dbh->do(q/
+        CREATE TABLE story_tag (
+                story_tag_id    INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                story_id        INT UNSIGNED NOT NULL,
+                tag             VARCHAR(255) NOT NULL,
+                ord             SMALLINT UNSIGNED NOT NULL,
+                INDEX(tag),
+                INDEX(story_id, ord)
+        )
+    /);
+}
+
+sub per_installation {
+    my ($self, %args) = @_;
+
+    # nothing to do yet
+}
+
+1;
